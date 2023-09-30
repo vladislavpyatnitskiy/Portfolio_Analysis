0813b7f15e2e59e79f3f0968988b5d821f49a1ee
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-09-30 17:18:07 +0500

    Create Portfolio Treemap.R
    Added script for generating Treetop plot for Portfolio

---
+++ Plots/Scripts for plots/Portfolio Treemap.R
@@ -0,0 +1,29 @@
+# library
+library(treemap)
+
+# Function to plot portfolio treemap
+portfolio_treemap <- function(x){
+  
+  # Take total values
+  t_m_values <- df_portfolio[,3 + 3 * seq(31, from = 0)]
+  
+  # Give them names of the assets
+  colnames(t_m_values) <- colnames(df_portfolio[,1 + 3 * seq(31, from = 0)])
+  
+  # Create vector to contain names of securities
+  t_m_group <- c(colnames(t_m_values))
+  
+  # Create vector to contain total sum of securities
+  t_m_value <- c(t_m_values[nrow(t_m_values),])
+  
+  # Join vectors into data frame
+  t_m_data <- data.frame(t_m_group, t_m_value)
+  
+  # Plot
+  treemap(t_m_data,
+          index="t_m_group",
+          vSize="t_m_value",
+          type="index")
+}
+# Test
+portfolio_treemap(df_portfolio)
