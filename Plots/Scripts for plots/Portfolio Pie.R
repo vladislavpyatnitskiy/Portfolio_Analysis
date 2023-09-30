8e6b1bb220067af490a461ff3494b2f59034fec6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-09-30 23:35:22 +0500

    Create Portfolio Pie.R
    Added script for Portfolio Pie

---
+++ Plots/Scripts for plots/Portfolio Pie.R
@@ -0,0 +1,30 @@
+# Function to facilitate Pie Plot generation
+portfolio_piePlot <- function(x, main = NULL, sub = NULL){
+  
+  # Subset tickers from price columns
+  tickers <- colnames((x[,1 + 3 * seq(31, from = 0)]))
+  
+  # Calculate portions
+  pct <- as.data.frame(x[,3 + 3 * seq(31, from = 0)]) / x[,ncol(x)]
+  
+  # Select last period and transform portions into percentages
+  pct <- as.numeric((round(pct[nrow(pct),] * 100)))
+  
+  # Add colour range
+  colors37 = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d",
+               "#5a51dc","#d49f36","#552095","#507f2d","#db37aa","#84b67c",
+               "#a06fda","#df462a","#5b83db","#c76c2d","#4f49a3","#82702d",
+               "#dd6bbb","#334c22","#d83979","#55baad","#dc4555","#62aad3",
+               "#8c3025","#417d61","#862977","#bba672","#403367","#da8a6d",
+               "#a79cd4","#71482c","#c689d0","#6b2940","#d593a7","#895c8b",
+               "#bd5975")
+  
+  # Pie Chart
+  pie(pct,
+      labels = c(sprintf("%s %s%%", tickers, pct)),
+      col = colors37,
+      main = main,
+      sub = sub) 
+}
+# Test
+portfolio_piePlot(df_portfolio, main = "Portfolio")
