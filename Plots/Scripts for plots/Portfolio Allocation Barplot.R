d18a43de09eddb093cd2e1862c26de8195fc0dd0
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-19 15:02:03 +0500

    Update Portfolio Allocation Barplot.R
    Upgraded script:
    
    • Changed dotted line type
    • Added percentages to numbers for mean and median

--- Plots/Scripts for plots/Portfolio Allocation Barplot.R
+++ Plots/Scripts for plots/Portfolio Allocation Barplot.R
@@ -10,7 +10,7 @@ weights_brplt <- function(x, main = "Portfolio", col = "blue"){
   # Select last period and transform portions into percentages
   pct <- as.numeric(((pct[nrow(pct),] * 100)))
   
-   # Create barplot
+  # Create barplot
   barplot(pct,
           names.arg = tickers,
           horiz = F,
@@ -20,27 +20,28 @@ weights_brplt <- function(x, main = "Portfolio", col = "blue"){
           ylim = c(0, round(max(pct)) + 0.5),
           las = 2 # Turn tickers 90º left
   )
+  
   # Y axis
   axis(side=2, at=seq(0, round(max(pct)), 0.5), las = 1,
-       seq(0.0, round(max(pct),1), 0.5))
+       labels = seq(round(0, 1), round(max(pct),1), 0.5))
   
   # Add grey lines for fast visual percentage calculation
   for (n in seq(0, round(max(pct)), 0.5)){ abline(h = n, col = "grey",
-                                                  lty = 2) }
+                                                  lty = 3) }
   
   # Add line for mean percentage
   abline(h = mean(pct), col = "red", lwd = 3)
   
   # Add line for median percentage
   abline(h = median(pct), col = "green", lwd = 3)
-
+  
   # Add legend with mean and median
-  legend("topright", legend = c((paste("Mean:",mean(pct))), paste("Median:",
-                                                      round(median(pct),3))),
+  legend("topright", legend = c((sprintf("Mean:    %s %%", mean(pct))),
+                                sprintf("Median: %s %%", round(median(pct),3))),
          fill = c("red", "green"), cex = 0.75)
   
   # Set up borders
   box()
 }
 # Test
-weights_brplt(df_portfolio, main = "Portfolio Allocation")
+weights_brplt(df_portfolio, main = "Portfolio Weights Allocation")
