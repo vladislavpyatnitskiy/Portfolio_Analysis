1eb4e0bfb99eb712630ee8383b534c2ec89482e5
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-28 19:25:39 +0500

    Update Barplot for portfolio.R
    Optimised script:
    
    • Reduced excessive lines and comments
    • Reduced script for generating vertical dotted lines
    • Reduced lines for vertical lines

--- Plots/Scripts for plots/Barplot for portfolio.R
+++ Plots/Scripts for plots/Barplot for portfolio.R
@@ -1,11 +1,9 @@
 # Function to generate barplot
 p.bar.plt <- function(x, main = NULL, col = "blue", v.bar.plt = NULL){
   
-  # Subset values with secuirities' values
-  x <- x[,1 + 3 * seq(31, from = 0)]
+  x <- x[,1 + 3 * seq(31, from = 0)] # Subset values with secuirities' values
   
-  # For each column in data set
-  for (n in 1:ncol(x)){ s <- x[,n]
+  for (n in 1:ncol(x)){ s <- x[,n] # For each column in data set
     
     # Clean data from NA & zeros and calculate logs
     security <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
@@ -22,35 +20,24 @@ p.bar.plt <- function(x, main = NULL, col = "blue", v.bar.plt = NULL){
   v.bar.plt <- sort(as.numeric(v.bar.plt), decreasing = T)
   
   # Create barplot
-  barplot(v.bar.plt,
-          names.arg = p.tickers,
-          horiz = T,
-          las= 1,
-          col = col,
-          main = main,
-          xlab = "Returns",
-          ylab = "",
-          xlim = c(round(min(v.bar.plt), 1), round(max(v.bar.plt), 1)))
+  bar.plt.script <- barplot(v.bar.plt,
+                            names.arg = p.tickers,
+                            horiz = T,
+                            las= 1,
+                            col = col,
+                            main = main,
+                            xlab = "Returns",
+                            ylab = "",
+                            xlim = c(round(min(v.bar.plt), 1),
+                                     round(max(v.bar.plt), 1)))
+  # Add grey dotted lines
+  abline(h = bar.plt.script, col = "grey",lty = 3) # through bars
   
-  # Create line going through bars to facilitate link between bar & ticker
-  abline(h = barplot(v.bar.plt,
-                     names.arg = p.tickers,
-                     horiz = T,
-                     las=1,
-                     col = col,
-                     main = main,
-                     xlab = "Returns",
-                     ylab = "",
-                     xlim = c(round(min(v.bar.plt), 1),
-                              round(max(v.bar.plt), 1))),col = "grey",lty = 3)
+  p.seq <- seq(round(min(v.bar.plt), 1), round(max(v.bar.plt), 1), by = .1)
   
-  # Add grey lines for fast visual percentage calculation
-  for (n in seq(round(min(v.bar.plt), 1), round(max(v.bar.plt), 1), by = .1)){ 
-    abline(v = n, col = "grey", lty = 3) }
+  for (n in p.seq){ abline(v = n, col = "grey", lty = 3) } # vertical bars
   
-  # Modify axis so intervals between them is 0.1
-  axis(side = 1, at = seq(round(min(v.bar.plt), 1),
-                          round(max(v.bar.plt), 1), by = .1), las = 1)
+  axis(side = 1, at = p.seq, las = 1) # configure x axis
   
   box() # Make borders for plot
 }
