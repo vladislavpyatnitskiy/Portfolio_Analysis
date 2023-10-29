c3888aa9f6558daf887a4de434dad0054b414fa1
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-29 16:21:26 +0500

    Update Portfolio Correlation Heatmap.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined operations
    • Added option to choose correlation values size of numbers
    • Renamed variables

--- Plots/Scripts for plots/Portfolio Correlation Heatmap.R
+++ Plots/Scripts for plots/Portfolio Correlation Heatmap.R
@@ -1,107 +1,58 @@
 # Function to generate heatmap for portfolio's correlations
-heatmap_for_correlation <- function(values_for_cor,
-                                    start_date = as.Date(Sys.Date()) - 365,
-                                    end_date = Sys.Date()){
+p.heatmap.plt <- function(data, s.d=as.Date(Sys.Date())-365, e.d=Sys.Date()){
   
-  # Select columns with prices
-  portfolio_tickers <- colnames(values_for_cor[,1 + 3 * seq(31, from = 0)])
+  p.tickers <- colnames(data[,1 + 3 * seq(31, from = 0)]) # ticker names
   
-  # Create an empty variable
-  asset_prices <- NULL
+  a.prices <- NULL # Create an empty variable
   
-  # Loop for data extraction
-  for (Ticker in portfolio_tickers)
+  for (Ticker in p.tickers) # Loop for data extraction
     
     # When neither start date nor end date are defined
-    asset_prices <- cbind(asset_prices,
-                               getSymbols(Ticker,
-                               from = start_date,
-                               to = end_date,
-                               src = "yahoo",
-                               auto.assign=FALSE)[,4]
-    )
+    a.prices <- cbind(a.prices,
+                      getSymbols(Ticker,
+                                 from = s.d,
+                                 to = e.d,
+                                 src = "yahoo",
+                                 auto.assign=FALSE)[,4])
+  # Eliminate NAs
+  a.prices<-a.prices[apply(a.prices,1,function(x) all(!is.na(x))),] 
   
-  # Get rid of NAs
-  asset_prices <- asset_prices[apply(asset_prices,1,
-                                     function(x) all(!is.na(x))),]
-  # Put the tickers in data set
-  colnames(asset_prices) <- portfolio_tickers
+  colnames(a.prices) <- p.tickers # Put the tickers in data set
   
-  # Make data discrete
-  asset_Returns <- ROC(asset_prices, type = "discrete")
+  asset_Returns <- diff(log(as.timeSeries(a.prices)))[-1,] # Calculate returns
   
-  # Make it time series
-  asset_Returns <-as.timeSeries(asset_prices)
+  m.correlation = as.matrix(asset_Returns) # Convert data into matrix
   
-  # Calculate asset returns
-  asset_Returns <- diff(log(asset_Returns))[-1,]
+  c.correlation = ncol(m.correlation) # Get number of columns
   
-  # Convert data into matrix
-  matrix_for_correlation = as.matrix(asset_Returns)
-  
-  # Get number of columns
-  column_for_correlation = ncol(matrix_for_correlation)
-  
-  # Cut column and row names if there are longer than 4 characters
-  names_for_correlation = abbreviate(colnames(matrix_for_correlation), 4)
-  
-  # Calculate correlation coefficients
-  new_cor <- cor(matrix_for_correlation)
+  new_cor <- cor(m.correlation) # Calculate correlation coefficients
   
   # Create appropriate colour for each pair of correlation for heatmap
-  ncolors_for_cor <- 10 * length(unique(as.vector(new_cor)))
-  k_for_corr <- round(ncolors_for_cor/2)
-  r_for_corr <- c(rep(0, k_for_corr), seq(0, 1, length = k_for_corr))
-  g_for_corr <- c(rev(seq(0, 1, length = k_for_corr)), rep(0, k_for_corr))
-  b_for_corr <- rep(0, 2 * k_for_corr)
-  corrColorMatrix <- (rgb(r_for_corr, g_for_corr, b_for_corr))
-  
+  k.c <- round((10 * length(unique(as.vector(new_cor))))/2)
+  corrColorMatrix <- rgb(c(rep(0, k.c), seq(0, 1, length = k.c)),
+                         c(rev(seq(0,1,length=k.c)), rep(0,k.c)), rep(0,2*k.c))
   # Display heatmap
-  image(x = 1:column_for_correlation,
-        y = 1:column_for_correlation,
-        z = new_cor[, column_for_correlation:1],
-        col = corrColorMatrix,
-        axes = FALSE, main = "",
-        xlab = "",
-        ylab = "")
+  image(x = 1:c.correlation,y = 1:c.correlation,z = new_cor[, c.correlation:1],
+        col = corrColorMatrix, axes = FALSE, main = "", xlab = "", ylab = "")
   
   # Add labels for both axis
-  axis(2, at = column_for_correlation:1,
-       labels = colnames(matrix_for_correlation), las = 2)
+  axis(2, at = c.correlation:1, labels = colnames(m.correlation), las = 2)
+  axis(1, at = 1:c.correlation, labels = colnames(m.correlation), las = 2)
   
-  axis(1, at = 1:column_for_correlation,
-       labels = colnames(matrix_for_correlation), las = 2)
+  title(main = "Portfolio Correlations Heatmap") # Add title for heatmap
   
-  # Add title for heatmap
-  title(main = "Heatmap for Portfolio's Correlations")
-  
-  # Box heatmap
-  box()
+  box() # Box heatmap
   
   # Add correlation values as text strings to each heatmap cell
-  x = y = 1:column_for_correlation
+  x = y = 1:c.correlation
   n_x = n_y = length(y)
-  xoy = cbind(rep(x, n_y),
-              as.vector(matrix(y,
-                               n_x,
-                               n_y,
-                               byrow = TRUE)))
-  coord_for_corr = matrix(xoy,
-                          n_x * n_y,
-                          2,
-                          byrow = FALSE)
+  xoy = cbind(rep(x, n_y), as.vector(matrix(y, n_x, n_y, byrow = TRUE)))
+  coord_for_corr = matrix(xoy, length(y) ^ 2, 2, byrow = FALSE)
   X_for_corr = t(new_cor)
-  for (i in 1:(column_for_correlation * column_for_correlation)) {
-    text(coord_for_corr[i, 1],
-         coord_for_corr[column_for_correlation *
-                          column_for_correlation + 1 - i,
-                        2],
-         round(X_for_corr[coord_for_corr[i,1],
-                          coord_for_corr[i,2]],
-               digits = 2),
-         col = "white",
-         cex = 0.5)
-  }
+  for (i in 1:c.correlation ^ 2) {
+    text(coord_for_corr[i, 1], coord_for_corr[c.correlation ^ 2 + 1 - i, 2],
+         round(X_for_corr[coord_for_corr[i,1],coord_for_corr[i,2]],digits = 2),
+         col = "white", cex = size) }
 }
 # Test
-heatmap_for_correlation(values_for_cor = df_portfolio)
+p.heatmap.plt(data = df_portfolio, size =.9)
