c4c64176398a0102b9c87b93eb156dbd13ff31c5
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-31 17:00:10 +0500

    Update Portfolio Treemap.R
    Optimised script:
    
    • Renamed variables
    • Combined operations
    • Reduced excessive lines
    • Edited comments

--- Plots/Scripts for plots/Portfolio Treemap.R
+++ Plots/Scripts for plots/Portfolio Treemap.R
@@ -2,66 +2,36 @@
 library(treemap)
 
 # Function to plot portfolio treemap
-portfolio_treemap3 <- function(x){
+p.treemap <- function(x){ v <- x[,3 + 3 * seq(31, from = 0)] # Total sum
   
-  # Take total values
-  t_m_values <- x[,3 + 3 * seq(31, from = 0)]
+  colnames(v) <- colnames(x[,1 + 3 * seq(31, from = 0)]) # Names of the assets
   
-  # Give them names of the assets
-  colnames(t_m_values) <- colnames(x[,1 + 3 * seq(31, from = 0)])
+  g <- c(colnames(v)) # Vector to contain names of securities
   
-  # Create vector to contain names of securities
-  t_m_group <- c(colnames(t_m_values))
+  v <- c(v[nrow(v),]) # Create vector to contain total sum of securities
   
-  # Create vector to contain total sum of securities
-  t_m_value <- c(t_m_values[nrow(t_m_values),])
+  r <- x[,3 + 3 * seq(31, from = 0)]/x[,2 + 3 * seq(31, from = 0)] # Mean Sum
   
-  #
-  names_for_treemap <- colnames(x[,1 + 3 * seq(31, from = 0)])
+  l <- NULL # Create empty variable to contain values
   
-  # Subset values with secuirities' values
-  new_s_v <- x[,3 + 3 * seq(31, from = 0)] / x[,2 + 3 * seq(31, from = 0)]
-  
-  # Create empty variable to contain values
-  values_for_brplt <- NULL
-  
-  # For each column in data set
-  for (n in 1:ncol(new_s_v)){
+  for (n in 1:ncol(r)){ s <- r[,n] # Return calculation
     
-    # Values for security
-    security <- new_s_v[,n]
-    
-    # Find zeros in data set
-    security2 <- security[!is.nan(security)]
-    
-    # Calculate logs
-    security2 <- diff(log(security2))[-1]
-    
-    # Calculate return for the ownership period
-    security2 <- round((exp(sum(security2)) - 1) * 100, 2)
-    
-    # Put in newly created variable
-    values_for_brplt <- rbind(values_for_brplt, security2)
-  }
+    l <- rbind(l, round((exp(sum(diff(log(s[!is.nan(s)]))[-1]))-1)*100,2)) }
   
-  # Give column names
-  rownames(values_for_brplt) <- colnames(t_m_values)
+  rownames(l) <- colnames(v) # Give column names
   
-  # Join vectors into data frame
-  t_m_data <- data.frame(t_m_group, t_m_value, values_for_brplt)
+  p.data <- data.frame(g, v, l) # Join vectors into data set
   
-  # Values in rectangles
-  t_m_data$label <- sprintf("%s: %s %%", t_m_data$t_m_group,
-                          t_m_data$values_for_brplt)
+  p.data$label <- sprintf("%s: %s %%", p.data$g, p.data$l) # Values in squares
   
   # Plot
-  treemap(t_m_data,
+  treemap(p.data,
           index=c("label"),
-          vSize="t_m_value",
+          vSize="v",
           type="index",
-          vColor="values_for_brplt",
+          vColor="l",
           title = "Portfolio Securities",
           title.legend = "Return")
 }
 # Test
-portfolio_treemap3(df_portfolio)
+p.treemap(df_portfolio)
