7a51c407ab0b84c81bf7c5fe51ddf728b90a2b93
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-04 19:40:56 +0500

    Update Portfolio Returns Calculation.R
    Debugged script by assigning time series from internal data frame rather than external one

--- Scripts/Portfolio Returns Calculation.R
+++ Scripts/Portfolio Returns Calculation.R
@@ -1,6 +1,6 @@
 # Function to get portfolio returns
 portfolio_returns <- function(x){ x <- x[,3 + 3 * seq(31, from = 0)] #x
-  
+
   x1 <- t(x) # Transpose x1 and x
   
   colnames(x1) <- rownames(x) # Make dates as column names x1 and x
@@ -9,16 +9,15 @@ portfolio_returns <- function(x){ x <- x[,3 + 3 * seq(31, from = 0)] #x
   
   # Loop for portfolio log returns calculation
   for (n in 2:ncol(x1)){ df2p <- x1[,(n-1):n] # x1 # Select two periods
-    
+  
     s <- df2p[apply(df2p, 1, function(row) all(row !=0 )),] # Remove zeros & NA
     
     # Add newly generated variable to data frame
     r <- rbind(r, log(as.numeric(colSums(s)[2]) / as.numeric(colSums(s)[1]))) }
   
   colnames(r) <- "Returns" # Give name to column
-  rownames(r) <- rownames(df_exp_for_logs) # Return dates to index
+  rownames(r) <- rownames(x) # Return dates to index
   
   as.timeSeries(r) # Make it time series
-}
-# Test
-returns_df <- portfolio_returns(df_portfolio)
+} 
+returns_df <- portfolio_returns(df_portfolio) # Test
