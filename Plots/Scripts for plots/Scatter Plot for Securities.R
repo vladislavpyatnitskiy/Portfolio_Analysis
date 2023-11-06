0c938a444132d40c8d5e073c8f3cc876464580ca
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-06 23:08:52 +0500

    Update Scatter Plot for Securities.R
    Reduced excessive lines

--- Plots/Scripts for plots/Scatter Plot for Securities.R
+++ Plots/Scripts for plots/Scatter Plot for Securities.R
@@ -1,41 +1,32 @@
 # Libraries
-lapply(c("quantmod", "timeSeries", "ggplot2", "ggrepel"),
-       require, character.only = TRUE)
+lapply(c("quantmod","timeSeries","ggplot2","ggrepel"),require,character.only=T)
 
 # Scatter plot of portfolio's securities
 p.scatter.plt <- function(x, main = NULL, xlab = NULL, ylab = NULL){
   
-  # Select securities columns
-  x <- x[,1 + 3 * seq(31, from = 0)]
+  x <- x[,1 + 3 * seq(31, from = 0)] # Select securities columns
   
-  # Empty variable to contain values
-  df.scatter <- NULL
+  df.scatter <- NULL # Empty variable to contain values
   
-  # For each security in data frame
-  for (n in 1:ncol(x)){ s <- x[,n]
-    
+  for (n in 1:ncol(x)){ s <- x[,n] # For each security in data frame
+  
     # Clean data to reduce NA and calculate return for ownership period  
     s.adj <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
     
     # Join standard deviation with return
     v.scatter <- cbind(sd(s.adj) * 1000, (exp(sum(s.adj)) - 1) * 100)
     
-    # Give column and row names to data frame
-    colnames(v.scatter) <- c("Risk", "Return")
-    rownames(v.scatter) <- colnames(s)
+    colnames(v.scatter) <- c("Risk", "Return") # Column names
+    rownames(v.scatter) <- colnames(s) # Row names
     
     # Join measures for all tickers
     df.scatter <- rbind.data.frame(df.scatter, v.scatter) }
-  
+    
   # Plot
-  ggplot(df.scatter, 
-         mapping = aes(x = df.scatter[,1],
-                       y = df.scatter[,2])) +
+  ggplot(df.scatter, mapping = aes(x = df.scatter[,1], y = df.scatter[,2])) +
     geom_point() +
     geom_text_repel(aes(label = rownames(df.scatter))) +
-    labs(title = main,
-         x = xlab,
-         y = ylab) +
+    labs(title = main, x = xlab, y = ylab) +
     geom_smooth(method='lm', se=FALSE, col = "red")
 }
 # Test
