223b3a84990c62f1d5931ebbf2ab173fc9a4a49a
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-12 12:57:46 +0500

    Update Scatter Plot for Securities.R
    Reduced excessive lines & edited comments & added white background

--- Plots/Scripts for plots/Scatter Plot for Securities.R
+++ Plots/Scripts for plots/Scatter Plot for Securities.R
@@ -9,25 +9,22 @@ p.scatter.plt <- function(x, main = NULL, xlab = NULL, ylab = NULL){
   df.scatter <- NULL # Empty variable to contain values
   
   for (n in 1:ncol(x)){ s <- x[,n] # For each security in data frame
-  
+    
     # Clean data to reduce NA and calculate return for ownership period  
     s.adj <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
     
-    # Join standard deviation with return
+    # Join standard deviation and return
     v.scatter <- cbind(sd(s.adj) * 1000, (exp(sum(s.adj)) - 1) * 100)
     
-    colnames(v.scatter) <- c("Risk", "Return") # Column names
-    rownames(v.scatter) <- colnames(s) # Row names
+    rownames(v.scatter) <- colnames(s) # Give row names to data frame
     
-    # Join measures for all tickers
-    df.scatter <- rbind.data.frame(df.scatter, v.scatter) }
-    
+    df.scatter <- rbind.data.frame(df.scatter, v.scatter) } # Join measures
+  
   # Plot
   ggplot(df.scatter, mapping = aes(x = df.scatter[,1], y = df.scatter[,2])) +
-    geom_point() +
-    geom_text_repel(aes(label = rownames(df.scatter))) +
-    labs(title = main, x = xlab, y = ylab) +
-    geom_smooth(method='lm', se=FALSE, col = "red")
+    geom_point() + theme_minimal() + geom_smooth(method='lm',se=F,col="red") +
+    geom_text_repel(aes(label=rownames(df.scatter))) +
+    labs(title=main,x=xlab,y=ylab)
 }
 # Test
 p.scatter.plt(df_portfolio, main = "Securities Performance",
