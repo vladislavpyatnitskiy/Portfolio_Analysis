f145eba5cf20236dd65802f6c4b27fb4b327ca9d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-13 19:35:12 +0500

    Update Portfolio Weights Matrix Sample.R
    Edited comments & Renamed variables

--- Scripts/Portfolio Measures/Portfolio Weights Matrix Sample.R
+++ Scripts/Portfolio Measures/Portfolio Weights Matrix Sample.R
@@ -1,23 +1,17 @@
 # Function to make matrix of portfolio weights
-pw_calculator <- function(asset_names, portfolio_weights){
+p.weights.calculator <- function(a.names, p.weights){
   
   # Create a matrix to store the products of weights
-  weight_product_matrix <- matrix(0, nrow = length(asset_names),
-                                  ncol = length(asset_names))
+  weight_product_matrix <- matrix(0,nrow=length(a.names),ncol=length(a.names))
   
   # Fill the matrix with weight products
-  for (i in 1:length(asset_names)) {
-    for (j in 1:length(asset_names)) {
-      weight_product_matrix[i, j] <- portfolio_weights[i] *
-        portfolio_weights[j] } }
+  for (i in 1:length(a.names)) { for (j in 1:length(a.names)) {
+      weight_product_matrix[i, j] <- p.weights[i] * p.weights[j] } }
   
-  # Set row and column names
-  rownames(weight_product_matrix) <- asset_names
-  colnames(weight_product_matrix) <- asset_names
+  rownames(weight_product_matrix) <- a.names # Set row and column names
+  colnames(weight_product_matrix) <- a.names
   
-  # Print the matrix
-  return(weight_product_matrix)
+  return(weight_product_matrix) # Print the matrix
 }
 # Test
-pw_calculator(asset_names = c("AAPL", "MSFT", "GOOGL"),
-              portfolio_weights = c(0.4, 0.3, 0.2))
+p.weights.calculator(a.names = c("AAPL","MSFT","GOOGL"),p.weights=c(.4,.3,.2))
