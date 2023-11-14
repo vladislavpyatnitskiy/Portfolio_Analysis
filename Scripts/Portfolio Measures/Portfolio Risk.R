7dfb9d4b7d75ef0c932db0734531d685366f13f9
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-14 14:22:53 +0500

    Update Portfolio Risk.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined operations
    • Edited comments
    • Renamed variables

--- Scripts/Portfolio Measures/Portfolio Risk.R
+++ Scripts/Portfolio Measures/Portfolio Risk.R
@@ -1,44 +1,32 @@
 # Function to calculate portfolio risk
-portfolio_risk <- function(x, portfolio_weights, lg = T, sqrt = T){
+portfolio_risk <- function(x, p.weights, lg = T, sqrt = T){
   
-  # Make logs if needed
-  if (isTRUE(lg)) { x = diff(log(x))[-1,] }
-  
-  # Calculate variances
-  test_variance <- as.matrix(apply(x, 2, function(col) var(col)))
+  if (isTRUE(lg)) { x = diff(log(x))[-1,] } # Make logs if needed
   
   # Calculate sum of products for variances and weights
-  var_prod <- crossprod(x = test_variance, y = portfolio_weights ^ 2)
+  v.pd<-crossprod(x=as.matrix(apply(x,2,function(col) var(col))),y=p.weights^2)
   
-  # Assign names
-  asset_names <- colnames(x)
+  a.names <- colnames(x) # Assign names
   
   # Create a matrix to store the products of weights
-  weight_product_matrix <- matrix(0, nrow = length(asset_names),
-                                  ncol = length(asset_names))
+  weight_product_matrix <- matrix(0, nrow=length(a.names),ncol=length(a.names))
   
   # Fill the matrix with weight products
-  for (i in 1:length(asset_names)) {
-    for (j in 1:length(asset_names)) {
-      weight_product_matrix[i, j] <- portfolio_weights[i] *
-        portfolio_weights[j] } }
+  for (i in 1:length(a.names)) { for (j in 1:length(a.names)) {
+    weight_product_matrix[i, j]<-p.weights[i] * p.weights[j] } }
   
-  # Set row and column names
-  rownames(weight_product_matrix) <- asset_names
-  colnames(weight_product_matrix) <- asset_names
+  rownames(weight_product_matrix) <- a.names # Row Names
+  colnames(weight_product_matrix) <- a.names # Column Names
   
   # Put weights and covariances in nested list
   list_wghts_cov <- list(weight_product_matrix, cov(x))
   
-  # Create an empty variable to put covariances and weights
-  wghts_cov <- NULL
+  wghts_cov <- NULL # Create an empty variable to put covariances and weights
   
-  # For each table in tables
-  for (n in 1:length(list_wghts_cov)){
-  
+  for (n in 1:length(list_wghts_cov)){ # For each table in tables
+    
     # Extract unique pairs and their correlations
-    cor_pairs <- which(upper.tri(list_wghts_cov[[n]],
-                                 diag = TRUE), arr.ind = TRUE)
+    cor_pairs <- which(upper.tri(list_wghts_cov[[n]], diag = T), arr.ind = T)
     
     # Put them into one data frame
     unique_pairs <- data.frame(
@@ -55,32 +43,20 @@ portfolio_risk <- function(x, portfolio_weights, lg = T, sqrt = T){
     different_tickers_pairs$Pair <- paste(different_tickers_pairs$Variable1,
                                           different_tickers_pairs$Variable2)
     
-    # Create new tables where put pairs in first column and values in second
-    new_data_set <- different_tickers_pairs[,4:3]
+    # Matrix with values of unique pairs
+    new_data_set <- as.data.frame(different_tickers_pairs[,4:3]) 
     
-    # Transform into data frame format
-    new_data_set <- as.data.frame(new_data_set)
-    
-    # When working with first table assign to empty variable
+    # Put values into table
     if (is.null(wghts_cov)){ wghts_cov <- new_data_set } else {
       
-      # Otherwise perform inner join
-      wghts_cov_join <- merge(x = wghts_cov, y = new_data_set, by = "Pair")  }
-  }
+      wghts_cov_join <- merge(x = wghts_cov, y = new_data_set, by = "Pair")} }
   
   # Sum of products for covariances and weights
-  cov_prod <- crossprod(x = wghts_cov_join[,2], y = wghts_cov_join[,3]) * 2
+  cov.pd <- crossprod(x = wghts_cov_join[,2], y = wghts_cov_join[,3]) * 2
   
-  # Portfolio's standard deviation
-  if (isTRUE(sqrt)) { portfolio_risk <- (var_prod + cov_prod) ^ 0.5 } else {
-    
-    # Portfolio's variance
-    portfolio_risk <- var_prod + cov_prod
-  }
+  # Portfolio's standard deviation & Portfolio's variance
+  if (isTRUE(sqrt)) { p.risk<-(v.pd + cov.pd)^.5 } else { p.risk<-v.pd+cov.pd } 
   
-  # Print the matrix
-  return(portfolio_risk)
+  return(p.risk) # Print the matrix
 }
-# Test
-portfolio_risk(x = stock_data, portfolio_weights = c(0.4, 0.3, 0.2, 0.1),
-               sqrt = T)
+portfolio_risk(x = stock_data, p.weights = c(.4, .3, .2, .1), sqrt = T) # Test
