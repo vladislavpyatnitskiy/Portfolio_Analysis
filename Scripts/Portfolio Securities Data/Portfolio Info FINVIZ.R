8043c9818a52e47b57d55f5a098b56676c55b501
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-03 12:57:46 +0500

    Update Portfolio Info FINVIZ.R
    Optimised script:
    
    • Reduced excessive lines and variables
    • Rewrote FOR loop

--- Scripts/Portfolio Securities Data/Portfolio Info FINVIZ.R
+++ Scripts/Portfolio Securities Data/Portfolio Info FINVIZ.R
@@ -1,41 +1,37 @@
 library("rvest") # Library 
+
 p.finviz.ratios <- function(x){ # Get info about tickers from FINVIZ
   
   x <- colnames(x[,1 + 3 * seq(31, from = 0)]) # portfolio tickers
   
-  df.f4 <- NULL
+  df_ <- NULL
   
   for (n in 1:length(x)){ j <- x[n] # Assign security and read HTML of page
   
     s <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", j))
     
-    s.yahoo <- s %>% html_nodes('table') %>% .[[10]] -> tab1 # Assign Table 
+    s.yahoo <- s %>% html_nodes('table') %>% .[[10]] -> tab # Assign Table 
     
-    s.header <- tab1 %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
     
-    df.f1 <- NULL # Create lists to contain values
-    df.f2 <- NULL
+    df <- NULL # Create lists to contain values
     
-    for (n in 0:(length(s.header) / 2)){ 
+    for (n in 0:(length(y) / 2)){ # Ratio values
       
-      df.f1 <- rbind(df.f1, s.header[(1 + n * 2)]) # Ratio names
-      
-      df.f2 <- rbind(df.f2, s.header[(2 + n * 2)]) } # Ratio values
+      df <- rbind(df, cbind(y[(1 + n * 2)], y[(2 + n * 2)])) } 
     
-    df.f3 <- data.frame(df.f1, df.f2) # Join 
+    df <- df[-nrow(df),] # Reduce last column
     
-    df.f3 <- df.f3[-nrow(df.f3),] # Reduce last column
+    df[27,1] <- "EPS next Y (%)" # Change name
     
-    df.f3[27,1] <- "EPS next Y (%)" # Change name
+    rownames(df) <- df[,1] # Assign row names
     
-    rownames(df.f3) <- df.f3[,1] # Assign row names
+    df <- subset(df, select = -c(1)) # Reduce excess column
     
-    df.f3 <- subset(df.f3, select = -c(1)) # Reduce excess column
+    colnames(df) <- j # Assign column name
     
-    colnames(df.f3) <- j # Assign column name
-    
-    if (is.null(df.f4)){ df.f4<-df.f3 } else { df.f4 <- cbind(df.f4,df.f3) } }
-    
-  df.f4 # Display
+    if (is.null(df_)){ df_ <- df } else { df_ <- cbind(df_, df) } }
+  
+  df_ # Display
 }
 p.finviz.ratios(df_portfolio) # Test
