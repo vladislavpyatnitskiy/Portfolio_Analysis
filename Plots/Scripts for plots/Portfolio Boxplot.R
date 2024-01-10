8f4d3371dd45966a37bce1a4b360a2317ecafd29
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-10 15:47:53 +0500

    Update Portfolio Boxplot.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined functions
    • Renamed variables
    • Edited comments

--- Plots/Scripts for plots/Portfolio Boxplot.R
+++ Plots/Scripts for plots/Portfolio Boxplot.R
@@ -1,19 +1,17 @@
 # Function to create Boxplot for Portfolio
 p.box.plt <- function(x){ x <- x[,1 + 3 * seq(31, from = 0)] # Extract data
-  
-  v.box.plt <- NULL # Variable for values
+
+  v <- NULL # Variable for values
   
   for (n in 1:ncol(x)){ s <- x[,n] # For each column in data set
   
-    # Clean data from NA & zeros and calculate logs
-    security <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
+    # Clean data from NA & zeros and calculate logs & Put it in data frame
+    v <- cbind(v, diff(log(s[apply(s,1,function(row) all(row !=0 )),]))[-1,]) } 
     
-    v.box.plt <- cbind(v.box.plt, security) } # Put it in data frame
-
-  colnames(v.box.plt) <- colnames(x) # Give column names
+  colnames(v) <- colnames(x) # Give column names & generate plot
   
-  boxplot.matrix(v.box.plt, main = "Securities Performance", title = F, las = 2,
-                 col = "steelblue", ylab = "Returns") 
+  boxplot.matrix(v, main = "Securities Returns", col = "steelblue", title = F,
+                 las = 2, ylab = "Returns") 
   
   abline(h = 0, col = "grey", lty = 3) # Add horizontal line
 }
