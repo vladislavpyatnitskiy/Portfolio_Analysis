5c8ad64c8dec1a42a1445b5d9560353d8a89d7ed
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-01 17:07:46 +0500

    Update Portfolio Weight for potential securities.R
    Upgraded script:
    
    • Added option to get values for several securities simultaneously

--- Scripts/Portfolio Measures/Portfolio Weight for potential securities.R
+++ Scripts/Portfolio Measures/Portfolio Weight for potential securities.R
@@ -2,14 +2,22 @@ lapply(c("quantmod", "timeSeries"), require, character.only = T) # Libs
 
 p.portion <- function(x, y, q = 1){ # potential security portion in portfolio
   
-  p <- getSymbols(x, from=as.Date(Sys.Date()), src="yahoo", auto.assign=F)[,4]
+  l <- NULL
   
-  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
+  for (n in 1:length(x)){ m <- q[n]
   
-  p <- as.timeSeries(p) # Make it time series
+    p<-getSymbols(x[n],from=as.Date(Sys.Date()),src="yahoo",auto.assign=F)[,4]
+    
+    p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
+    
+    p <- as.timeSeries(p) # Make it time series
+    
+    l <- rbind(l, round(as.numeric(p*m/(as.numeric(y[nrow(y),ncol(y)]) + p*m)),
+                        4) * 100) }
   
-  p <- round(as.numeric(p * q / (as.numeric(y[nrow(y),ncol(y)]) + p * q)), 4)
+  colnames(l) <- "Weight (%)"
+  rownames(l) <- x
   
-  p # Display
+  l # Display
 }
-p.portion("X", df_portfolio, q = 1) # Test
+p.portion(c("X", "AMZN"), df_portfolio, q = c(1, 2)) # Test
