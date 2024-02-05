30717e533c5ce62b0cbce28d4b4fda2d59c6685c
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-05 13:39:55 +0500

    Update Portfolio Weight for potential securities.R
    Fixed bug leading to an error caused by dates

--- Scripts/Portfolio Measures/Portfolio Weight for potential securities.R
+++ Scripts/Portfolio Measures/Portfolio Weight for potential securities.R
@@ -5,8 +5,16 @@ p.portion <- function(x, y, q = 1){ # potential security portion in portfolio
   l <- NULL
   
   for (n in 1:length(x)){ m <- q[n]
+    
+    if (isTRUE(weekdays(as.Date(Sys.Date())) == "Monday")){ 
+      
+      d <- as.Date(Sys.Date()) - 2 }
   
-    p<-getSymbols(x[n],from=as.Date(Sys.Date()),src="yahoo",auto.assign=F)[,4]
+    if (isTRUE(weekdays(as.Date(Sys.Date())) == "Sunday")){ 
+      
+      d <- as.Date(Sys.Date()) - 1 }
+  
+    p <- getSymbols(x[n], from = d, src = "yahoo", auto.assign = F)[,4]
     
     p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
     
@@ -14,10 +22,10 @@ p.portion <- function(x, y, q = 1){ # potential security portion in portfolio
     
     l <- rbind(l, round(as.numeric(p*m/(as.numeric(y[nrow(y),ncol(y)]) + p*m)),
                         4) * 100) }
-  
+    
   colnames(l) <- "Weight (%)"
   rownames(l) <- x
   
   l # Display
 }
-p.portion(c("X", "AMZN"), df_portfolio, q = c(1, 2)) # Test
+p.portion(x = c("AMZN"), y = df_portfolio, q = c(1)) # Test
