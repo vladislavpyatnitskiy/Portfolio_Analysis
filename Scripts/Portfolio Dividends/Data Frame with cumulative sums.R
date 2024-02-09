603fd18500c24727f3b1c30b0664a395fab58b58
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-09 11:32:26 +0500

    Create Data Frame with cumulative sums.R
    Added script to generate Data Frame with cumulative sums of dividends

---
+++ Scripts/Portfolio Dividends/Data Frame with cumulative sums.R
@@ -0,0 +1,17 @@
+p.dividends.cum <- function(x){ # Data Frame with cumulative dividends
+  
+  s.names <- colnames(x[,1 + 3 * seq(31, from = 0)]) # Tickers
+  
+  x <- cumsum(x[,3 + 3 * seq(31, from = 0)]) # Cumulative total sum
+  
+  colnames(x) <- s.names # Assign tickers for columns
+  
+  x <- cbind(x, as.timeSeries(rowSums(x, na.rm = T))) # Sum dividends
+  
+  colnames(x)[ncol(x)] <- "Total" # Column name for cumulative sum
+  
+  x <- x[,colSums(x) !=0] # Subtract column of securities without dividends
+  
+  x # Display
+}
+p.dividends.cum(df_portfolio_dividend) # Test
