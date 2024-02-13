1f1a2ff49aaa6095cdd0fa0a78afd559a1f8a7eb
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-13 18:26:13 +0500

    Update Line Plot for Portfolio Dividends.R
    Updated script:
    
    • Made more detailed vertical axis
    • Made line red
    • Added dotted grey horizontal lines
    • Modified plot title

--- Plots/Scripts for plots/Line Plot for Portfolio Dividends.R
+++ Plots/Scripts for plots/Line Plot for Portfolio Dividends.R
@@ -1,8 +1,13 @@
-p.plt.dividends <- function(x){ # Plot showing Dividend Accumulation
+p.plt.dividends <- function(x, N){ # Plot showing Dividend Accumulation
   
   x <- cumsum(x[,ncol(x)]) # Calculate cumulative sum of dividends
   
-  plot(x, main = "Dividend Accumulation", sub = "Data Source: Yahoo! Finance",
-       xlab = "Trading Days", las = 1, ylab = "Amount in USD$")
+  plot(x, main = "Dividend Amount Accumulation", col="red",xlab="Trading Days",
+       sub="Data Source: Yahoo! Finance",lwd=3,las=1, ylab = "Amount in USD$")
+  
+  abline(h = 0) # Add black horizontal line showing 0 and grey line for others
+  abline(h = seq(x[nrow(x),], from = N, by = N), col = "grey", lty = 3)
+  
+  axis(side = 2, at = seq(x[nrow(x),], from = N, by = N * 2), las = 2) # Axis
 }
-p.plt.dividends(df_portfolio_dividend) # Test
+p.plt.dividends(df_portfolio_dividend, N = 10) # Test
