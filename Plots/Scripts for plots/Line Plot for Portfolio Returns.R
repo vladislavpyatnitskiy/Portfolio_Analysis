7ac497984f9b4f5f13e77519f130253634de49d1
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-13 19:59:11 +0500

    Update Line Plot for Portfolio Returns.R
    Upgraded script:
    
    • Colour of line changes regarding to positive or negative return 
    • Reduced repeating lines
    • Improved axis numbers and horizontal lines

--- Plots/Scripts for plots/Line Plot for Portfolio Returns.R
+++ Plots/Scripts for plots/Line Plot for Portfolio Returns.R
@@ -1,26 +1,29 @@
-# Return Plot
-p.plt <- function(x, SD = F){ if (isFALSE(SD)){
+p.plt <- function(x, SD = F){ if (isFALSE(SD)){ # Return Plot
   
-  x <- apply(x,2,function(col) (exp(cumsum(col))-1) * 100)
-
-  plot(x,main="Portfolio Performance",xlab="Trading Days",ylab = "Return (%)",
-       type="l", las=1) # Plot
-  
-  abline(h = 0, lwd = 3, col = "black") # Break Even
-  
-  abline(h = x[nrow(x),], col = "red", lwd = 2) # Current Return
-  
-  axis(side = 2, at=seq(-100, 100, 1), las = 1) # y axis
-  
-  for (n in seq(-100,100,1)){ abline(h=n,col="grey",lty=3) } } else { y<-x*100
+    x <- apply(x, 2, function(col) (exp(cumsum(col)) - 1) * 100) # Return
     
-    plot(y,main="Portfolio Volatility",xlab="Trading Days",type="l",las=1,
-         ylab="Fluctuations (%)", ylim = c(signif(min(y)), signif(max(y))))  
+    if (x[nrow(x),] > 0){ plot(x, main = "Portfolio Performance", type="l",
+                               las = 1, xlab = "Trading Days", lwd = 3,
+                               ylab = "Return (%)", col = "green4")
+      
+    } else { plot(x, main = "Portfolio Performance", xlab = "Trading Days", 
+                  ylab="Return (%)", type="l", las = 1, col = "red3",
+                  lwd = 3) }
     
-    abline(h = 0, lwd = 3, col = "black") # Break Even
+    abline(h = x[nrow(x),], col = "navy", lwd = 1) # Current Return
     
-    axis(side = 2, at=seq(-100, 100, 1), las = 1) # y axis
+    for (n in c(-99,-98,-97,-96)){ axis(side = 2, at=seq(n,max(x),5),las=1) }
     
-    for (n in seq(-100,100,1)){ abline(h = n,col = "grey",lty = 3) } }
+      } else { y <- x * 100
+    
+    plot(y, main = "Portfolio Return Volatility", xlab="Trading Days",las=1,
+         ylab = "Fluctuations (%)", ylim = c(signif(min(y)),signif(max(y))),
+         col = "black", type = "l")  
+    
+    for (n in c(-99)){ axis(side = 2, at = seq(n, max(y), 2), las = 1) } }
+    
+  abline(h = 0) # Break Even
+  for (n in seq(-100, -1, 1)){ abline(h = n, col = "grey", lty = 3) }
+  for (n in seq(1, 100, 1)){ abline(h = n, col = "grey", lty = 3) }
 }
-p.plt(returns_df, SD = T) # Test
+p.plt(returns_df, SD = F) # Test
