1a62dc9b239361c959924a459a247590f8e41893
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-15 21:34:05 +0500

    Update Portfolio Securities Market Cap with Levels.R
    Added option to choose between displaying Data Frame or general info

--- Scripts/Portfolio Securities Data/Portfolio Securities Market Cap with Levels.R
+++ Scripts/Portfolio Securities Data/Portfolio Securities Market Cap with Levels.R
@@ -1,10 +1,10 @@
 library("rvest") # Library
 
-p.marketcap <- function(x){ # Market Cap Info
+p.marketcap <- function(x, info = F){ # Market Cap Info
   
   x <- colnames(x[,1 + 3 * seq(31, from = 0)]) # tickers
   
-  df <- NULL
+  df <- NULL # Data Frame for Market Cap Levels and Values
   
   for (n in 1:length(x)){ v <- x[n] # Subset ticker
   
@@ -25,21 +25,63 @@ p.marketcap <- function(x){ # Market Cap Info
       
       s <- as.numeric(s[1,1]) * 1000 } else { s <- as.numeric(s[1,1]) }
     
-    if (s < .3){ l <- "Micro-Cap" } # if < $300 million => Micro-Cap
+    if (isFALSE(info)){ # Market Cap Levels
     
-    else if (s > .3 && s < 2) { l <- "Small-Cap" } # Small-Cap
+      if (s < .3){ l <- "Micro-Cap" } # if < $300 million => Micro-Cap
+      
+      else if (s > .3 && s < 2) { l <- "Small-Cap" } # Small-Cap
+      
+      else if (s > 2 && s < 10) { l <- "Mid-Cap" } # Mid-Cap
+      
+      else if (s > 10 && s < 200) { l <- "Large-Cap" } # Large-Cap
+      
+      else { l <- "Mega-Cap" } # if > $200 billion => Mega-Cap
+      
+      df <- rbind.data.frame(df, cbind(l, s)) } # Market Cap Level with values
     
-    else if (s > 2 && s < 10) { l <- "Mid-Cap" } # Mid-Cap
+    else { df <- rbind(df, s) } } # Market Cap values only
     
-    else if (s > 10 && s < 200) { l <- "Large-Cap" } # Large-Cap
+    if (isFALSE(info)){ # Create Data Frame
     
-    else { l <- "Mega-Cap" } # if > $200 billion => Mega-Cap
+      rownames(df) <- x # Tickers
+      colnames(df) <- c("Level", "Marker Cap ($billions)") # colnames
+      
+      df } else { c <- as.numeric(df) # Make available values to numeric format
     
-    df <- rbind.data.frame(df, cbind(l, s)) } # Data Frame 
+    names(c) <- x # Assign names to them
     
-  rownames(df) <- x # Tickers
-  colnames(df) <- c("Level", "Marker Cap ($billions)") # colnames
-  
-  df # Display
+    l <- NULL # Find securities without debt/ebitda values
+    
+    for (n in 1:length(c)){ if (is.na(c[n])){ l <- c(l, names(c)[n])} }
+    
+    c <- sort(c, decreasing = T) # Sort in a descending way
+    
+    m <- NULL # Write advices about securities according to Market Cap
+    
+    if (isFALSE(identical(names(which(c > 200)), character(0)))){
+      
+      m <- c(m, paste("Mega-Cap Companies:",
+                      toString(names(which(c > 200))))) }
+    
+    if (isFALSE(identical(names(which(c > 10 & c < 200)), character(0)))){
+      
+      m <- c(m, paste("Large-Cap Companies:",
+                      toString(names(which(c > 10 & c < 200))))) }
+    
+    if (isFALSE(identical(names(which(c > 2 & c < 10)), character(0)))){
+      
+      m <- c(m, paste("Medium-Cap Companies:",
+                      toString(names(which(c > 2 & c < 10))))) }
+    
+    if (isFALSE(identical(names(which(c > .3 & c < 2)), character(0)))){
+      
+      m <- c(m, paste("Small-Cap Companies:",
+                      toString(names(which(c > .3 & c < 2))))) }
+    
+    if (isFALSE(identical(names(which(c < .3)), character(0)))){
+      
+      m <- c(m, paste("Micro-Cap Companies:", toString(names(which(c < .3)))))}
+    
+    m } # Display
 }
-p.marketcap(df_portfolio) # Test
+p.marketcap(df_portfolio, info = T) # Test
