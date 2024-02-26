d10cad1e7a14538600d9aa72b636eacb0512d6a6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-26 10:35:43 +0500

    Update Portfolio Drawdown Plot.R
    Updated script:
    
    • Added grey horizontal dotted lines
    • Changed colour of line to red
    • Added option between fluctuations and cumulative returns
    • Substituted decimal numbers to whole ones for y axis

--- Plots/Scripts for plots/Portfolio Drawdown Plot.R
+++ Plots/Scripts for plots/Portfolio Drawdown Plot.R
@@ -1,21 +1,41 @@
-p.drawdown.plt <- function(x){ # Drawdown plot for portfolio
+p.drawdown.plt <- function(x, SD = T){ # Drawdown plot for portfolio
   
-  x <- apply(x,2,function(col) (exp(cumsum(col))-1) * 100) # Cumulative return
-  x[x > 0] <- 0 # Reduce postive values
+  if (isFALSE(SD)){ r <- rownames(x) # Save dates for Cumulative Returns
   
-  # For each column in data frame
-  for (n in 1:ncol(x)){ security <- x[,n]
+    x <- apply(x, 2, function(col) exp(cumsum(col)) - 1) # Cumulative Returns
+    
+    rownames(x) <- r } # Return dates as row names as they were vanished 
   
-    # Plot
-    plot(security,
-         type = "l",
-         main = sprintf("%s Drawdown", colnames(security)),
-         xlab = "Trading Days",
-         ylab = "Negative Return (%)",
-         las = 2,
-         ylim = c(signif(min(security), 1), 0)) }
+  x <- x * 100 # Multiply returns by 100
   
-  # Add horizontal lines
-  abline(h = seq(signif(min(security), 1), 0, 1), lty = 3, col = "grey")
+  x[x > 0] <- 0 # Replace positive values as 0
+  
+  for (n in 1:ncol(x)){ security <- x[,n] # Plot column in data frame
+    
+    if (isTRUE(SD)){ m <- "Drawdown Plot of %s Fluctuations" } else {
+      
+      m <- "Drawdown Plot of Cumulative %s" } # Title depending on SD
+  
+    plot(security, col = "red", ylim = c(min(security), 0), type = "l",
+         main = sprintf(m, colnames(security)), las = 2, xlab = "Trading Days",
+         ylab = "Negative Returns (%)")
+    
+    abline(h = 0) # Add horizontal line at break even
+    
+    if (abs(min(security)) < 10){ # Add grey horizontal dotted lines
+      
+      abline(h = seq(-10, -1, 1), lty = 3, col = "grey") } # 2
+    
+    else if (abs(min(security)) < 15){ # Add grey horizontal dotted lines
+      
+      abline(h = seq(-20, -2, 2), lty = 3, col = "grey") } # 2
+    
+    else if (abs(min(security)) > 15 && abs(min(security)) < 45){ # 5
+      
+      abline(h = seq(-50, -5, 5), lty = 3, col = "grey") }
+    
+    else if (abs(min(security)) > 45 && abs(min(security)) < 90){ # 10
+      
+      abline(h = seq(-100, -10, 10), lty = 3, col = "grey") } }
 }
-p.drawdown.plt(returns_df) # Test
+p.drawdown.plt(returns_df, SD = T) # Test
