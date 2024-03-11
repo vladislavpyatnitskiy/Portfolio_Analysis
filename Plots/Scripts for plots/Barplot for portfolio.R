6b492fadaa1886604330d270cdabdd7e3de9a169
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-03-11 14:28:00 +0500

    Update Barplot for portfolio.R
    Optimised script:
    
    • Negative returns shown red and positive show green
    • Axes are set up automatically
    • Title is more precise

--- Plots/Scripts for plots/Barplot for portfolio.R
+++ Plots/Scripts for plots/Barplot for portfolio.R
@@ -1,14 +1,12 @@
-# Function to generate barplot
-p.bar.plt <- function(x, main = NULL, col = "blue", v = NULL){
+p.bar.plt <- function(x, main = NULL, v = NULL){ # Bar Plot of Stocks Returns
   
-  x <- x[,1 + 3 * seq(31, from = 0)] # Subset values with secuirities' values
+  x <- x[,1 + 3 * seq(31, from = 0)] # Subset values with securities' values
   
-  for (n in 1:ncol(x)){ s <- x[,n] # For each column in data set
+  for (n in 1:ncol(x)){ s <- x[,n] # Clean data, calculate logs and returns
   
-    # Clean data from NA & zeros and calculate logs
     security <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
     
-    v <- cbind(v, exp(sum(security)) - 1) } # Calculate return
+    v <- cbind(v, (exp(sum(security)) - 1) * 100) } # Add value to Data Frame
     
   colnames(v) <- colnames(x) # Give column names
   
@@ -17,20 +15,27 @@ p.bar.plt <- function(x, main = NULL, col = "blue", v = NULL){
   
   v <- sort(as.numeric(v), decreasing = T) # Make data numeric and sort values
   
-  # Create barplot
-  plt <- barplot(v, names.arg = p.tickers, horiz = T, las= 1, col = col,
-                 main = main, xlab = "Returns", ylab = "",
-                 xlim = c(round(min(v), 1), round(max(v), 1)))
+  mx <- ceiling(round(max(v)) / 10 ^ (nchar(round(max(v))) - 1)) *
+    10 ^ (nchar(round(max(v))) - 1) # Round maximum value up
   
-  # Add grey dotted lines
-  abline(h = plt, col = "grey",lty = 3) # through bars
+  if (min(v) < 0){ # Round minimum value down
+    
+    if (any(seq(9) == as.numeric(strsplit(as.character(min(v)),"")[[1]][3]))){
+      
+      mn <- (as.numeric(strsplit(as.character(min(v)),"")[[1]][2]) + 1) * -10 }
+  }
+  # Bar Plot with securities returns
+  plt <- barplot(v, names.arg = p.tickers, horiz = T, xlab = "Returns (%)",
+                 main = "Performance of Portfolio Securities Returns",
+                 xlim = c(mn, mx),  las = 1,
+                 col=c(rep("green4",length(v)-sum(v<0)),rep("red3",sum(v<0)))) 
   
-  p.seq <- seq(round(min(v), 1), round(max(v), 1), by = .1)
+  # Add vertical (returns) and horizontal (through bars) grey dotted lines
+  abline(h = plt, col = "grey", lty = 3) 
+  for (n in seq(mn, mx, by = 10)){ abline(v = n, col = "grey", lty = 3) } 
   
-  for (n in p.seq){ abline(v = n, col = "grey", lty = 3) } # vertical bars
-  
-  axis(side = 1, at = p.seq, las = 1) # configure x axis
+  axis(side = 1, at = seq(mn, mx, by = 10), las = 1) # configure x axis
   
   box() # Make borders for plot
 }
-p.bar.plt(df_portfolio, main = "Securities Performance") # Test
+p.bar.plt(df_portfolio) # Test
