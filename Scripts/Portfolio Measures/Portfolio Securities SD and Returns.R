9a6651c019b8786a9f5bd22b580427d2f8b85d17
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-03-13 17:05:19 +0500

    Create Portfolio Securities SD and Returns.R
    Added script to calculate both standard deviations and returns of securities

---
+++ Scripts/Portfolio Measures/Portfolio Securities SD and Returns.R
@@ -0,0 +1,21 @@
+p.sd.return <- function(x){ # Data Frame with Standard Deviations and Returns
+  
+  tickers <- colnames(x[,1 + 3 * seq(31, from = 0)]) # Tickers
+  
+  x <- x[,1 + 3 * seq(31, from = 0)] # Select securities columns
+  
+  d <- NULL # Empty variable to contain values
+  
+  for (n in 1:ncol(x)){ s <- x[,n] # For each security in data frame
+  
+    # Clean data to reduce NA and calculate return for ownership period  
+    j <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
+    
+    d <- rbind.data.frame(d, cbind(sd(j)*1000, (exp(sum(j))-1)*100)) } # Join
+  
+  rownames(d) <- tickers # Tickers
+  colnames(d) <- c("Standard Deviation", "Return") # Column names
+  
+  d # Display
+}
+p.sd.return(df_portfolio) # Test
