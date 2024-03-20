28b3f649a46a771c37cabbb47d18b0a005c895f9
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-03-20 10:57:51 +0500

    Update Portfolio Boxplot.R
    Updated script:
    
    • Added both horizontal and vertical grey dotted lines
    • Change colour of break-even point (when h = 0) to black 
    • Added right y-axis
    • Increased number of values of y-axes

--- Plots/Scripts for plots/Portfolio Boxplot.R
+++ Plots/Scripts for plots/Portfolio Boxplot.R
@@ -7,12 +7,19 @@ p.box.plt <- function(x){ x <- x[,1 + 3 * seq(31, from = 0)] # Extract data
   
     # Clean data from NA & zeros and calculate logs & Put it in data frame
     v <- cbind(v, diff(log(s[apply(s,1,function(row) all(row !=0 )),]))[-1,]) } 
-    
+  
   colnames(v) <- colnames(x) # Give column names & generate plot
   
-  boxplot.matrix(v, main = "Securities Returns", col = "steelblue", title = F,
-                 las = 2, ylab = "Returns") 
+  boxplot.matrix(v, main = "Fluctuations of Portfolio Securities",
+                 col = "steelblue", title = F, las = 2, ylab = "Returns (%)") 
   
-  abline(h = 0, col = "grey", lty = 3) # Add horizontal line
+  for (n in 1:2){ axis(side = n * 2, at = seq(-1, 1, .1), las = 1) } # y-axis
+  
+  par(mar = c(5, 4, 4, 4)) # Define borders of the plot to fit right y-axis
+  
+  abline(v = seq(ncol(v)), col = "grey", lty = 3) # Add vertical lines
+  abline(h = 0, col = "black", lty = 3) # Add horizontal line at 0
+  abline(h = seq(-1, -.1, .1), col = "grey", lty = 3) # horizontal line < 0
+  abline(h = seq(.1, 1, .1), col = "grey", lty = 3) # horizontal line > 0
 }
 p.box.plt(df_portfolio) # Test
