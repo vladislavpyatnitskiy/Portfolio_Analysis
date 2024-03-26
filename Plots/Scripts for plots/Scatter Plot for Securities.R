613931e75eb895043b644b75908d4d08139d56c8
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-03-26 17:26:42 +0500

    Update Scatter Plot for Securities.R
    Updated script:
    
    • Move title to the middle of the plot
    • Removed regression line
    • Removed option to change title, xlab and ylab

--- Plots/Scripts for plots/Scatter Plot for Securities.R
+++ Plots/Scripts for plots/Scatter Plot for Securities.R
@@ -1,25 +1,23 @@
-# Libraries
-lapply(c("quantmod","timeSeries","ggplot2","ggrepel"),require,character.only=T)
+lapply(c("quantmod", "ggplot2", "ggrepel"), require, character.only = T) # Libs
 
-# Scatter plot of portfolio's securities
-p.scatter.plt <- function(x, main = NULL, xlab = NULL, ylab = NULL){
+p.scatter.plt <- function(x){ # Scatter plot of portfolio's securities
   
   x <- x[,1 + 3 * seq(31, from = 0)] # Select securities columns
   
   d <- NULL # Empty variable to contain values
   
   for (n in 1:ncol(x)){ s <- x[,n] # For each security in data frame
-  
+    
     # Clean data to reduce NA and calculate return for ownership period  
     j <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
     
     d <- rbind.data.frame(d, cbind(sd(j)*1000, (exp(sum(j))-1)*100)) } # Join
     
   # Plot
-  ggplot(d, mapping = aes(x=d[,1], y=d[,2])) + geom_point() + theme_minimal() +
-    geom_smooth(method = 'lm', se = F, col = "red") +
-    geom_text_repel(aes(label=colnames(x))) + labs(title=main,x=xlab,y=ylab)
+  ggplot(d, mapping = aes(x = d[,1], y = d[,2])) + geom_point() +
+    geom_text_repel(aes(label = colnames(x))) + theme_minimal() +
+    theme(plot.title = element_text(hjust = .5)) +
+    labs(title = "Performance of Portfolio Securities",
+         x = "Risk (Standard Deviation)", y = "Return (%)")
 }
-# Test
-p.scatter.plt(df_portfolio, main = "Securities Performance",
-              xlab = "Risk (Standard Deviation)", ylab = "Return (%)")
+p.scatter.plt(df_portfolio) # Test
