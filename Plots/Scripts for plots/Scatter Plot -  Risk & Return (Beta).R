f579687b7ecc0bfcd2e7464f76ab7b19ed6d29b9
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-03-26 18:52:51 +0500

    Update Scatter Plot -  Risk & Return (Beta).R
    Updated script:
    
    • Moved title to the middle of the plot
    • Removed regression line
    • Removed option to change title, xlab, ylab

--- Plots/Scripts for plots/Scatter Plot -  Risk & Return (Beta).R
+++ Plots/Scripts for plots/Scatter Plot -  Risk & Return (Beta).R
@@ -1,15 +1,14 @@
 lapply(c("quantmod","timeSeries","ggplot2","ggrepel", "rvest"),
        require,character.only=T)
 
-# Scatter plot of Portfolio Securities' Risk & Return (Beta) 
-p.scatter.plt.beta <- function(x, main = NULL, xlab = NULL, ylab = NULL){
+p.scatter.plt.beta <- function(x){ # Portfolio Securities' Risk & Return (Beta) 
   
   x <- x[,1 + 3 * seq(31, from = 0)] # Select securities columns
   
   d <- NULL # Empty variable to contain values
   
   for (n in 1:ncol(x)){ s <- x[,n] # For each security in data frame
-    
+  
     c <- colnames(s) #
     
     p <- sprintf("https://finance.yahoo.com/quote/%s/key-statistics?p=%s",c,c)
@@ -29,25 +28,24 @@ p.scatter.plt.beta <- function(x, main = NULL, xlab = NULL, ylab = NULL){
       for (m in l){ b <- cbind(b, getSymbols(m, from=as.Date(Sys.Date())-365*5,
                                              to=Sys.Date(), src="yahoo",
                                              auto.assign=F)[,4])}
-    
-      b <- b[apply(b, 1, function(x) all(!is.na(x))),] # Get rid of NA
       
-      b = diff(log(as.timeSeries(b)))[-1,] # Calculate Returns
-      
-      b <- as.numeric(apply(b[,1], 2,
-                            function(col) ((lm((col) ~
-                                                 b[,2]))$coefficients[2]))) }
-      
+        b <- b[apply(b, 1, function(x) all(!is.na(x))),] # Get rid of NA
+        
+        b = diff(log(as.timeSeries(b)))[-1,] # Calculate Returns
+        
+        b <- apply(b[,1], 2, function(col) ((lm((col) ~
+                                                  b[,2]))$coefficients[2])) }
+        
     # Clean data to reduce NA and calculate return for ownership period  
     j <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
     
     d <- rbind.data.frame(d, cbind(b, (exp(sum(j)) - 1) * 100)) } # Join
-  
+    
   # Plot
   ggplot(d, mapping = aes(x=d[,1], y=d[,2])) + geom_point() +
-    theme_minimal() + geom_smooth(method = 'lm', se = F, col = "red") +
-    geom_text_repel(aes(label=colnames(x))) + labs(title=main,x=xlab,y=ylab)
+    theme_minimal() + geom_text_repel(aes(label=colnames(x))) +
+    theme(plot.title = element_text(hjust = .5)) +
+    labs(title = "Performance of Portfolio Securities",
+         x = "Risk (Beta)", y = "Return (%)")
 }
-# Test
-p.scatter.plt.beta(df_portfolio, main = "Securities Performance",
-                   xlab = "Risk (Beta)", ylab = "Return (%)")
+p.scatter.plt.beta(df_portfolio) # Test
