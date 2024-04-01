81dd7f7914b77cc3fe1b5523571d2c9f45f7af5c
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-01 11:36:24 +0500

    Update Portfolio Returns Calculation.R
    Fixed issue function calling an error when number of columns is different from original portfolio data frame

--- Scripts/Portfolio Returns Calculation.R
+++ Scripts/Portfolio Returns Calculation.R
@@ -1,5 +1,6 @@
-# Function to get portfolio returns
-portfolio_returns <- function(x){ x <- x[,3 + 3 * seq(31, from = 0)] #x
+portfolio_returns <- function(x){ # Calculation of Portfolio Returns
+  
+  x <- x[,3 * seq(ncol(x) %/% 3, from = 1)] # Take columns with Total Sum
 
   x1 <- t(x) # Transpose x1 and x
   
@@ -9,12 +10,12 @@ portfolio_returns <- function(x){ x <- x[,3 + 3 * seq(31, from = 0)] #x
   
   # Loop for portfolio log returns calculation
   for (n in 2:ncol(x1)){ df2p <- x1[,(n-1):n] # x1 # Select two periods
-  
+    
     s <- df2p[apply(df2p, 1, function(row) all(row !=0 )),] # Remove zeros & NA
     
     # Add newly generated variable to data frame
     r <- rbind(r, log(as.numeric(colSums(s)[2]) / as.numeric(colSums(s)[1]))) }
-  
+    
   colnames(r) <- "Returns" # Give name to column
   rownames(r) <- rownames(x) # Return dates to index
   
