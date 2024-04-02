a6214b3a9f9001cb5054a1bfe339cc2b1de77262
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-02 12:01:32 +0500

    Update Scatter Plot for Securities.R
    Fixed bug leading to incorrect display of tickers

--- Plots/Scripts for plots/Scatter Plot for Securities.R
+++ Plots/Scripts for plots/Scatter Plot for Securities.R
@@ -2,17 +2,17 @@ lapply(c("quantmod", "ggplot2", "ggrepel"), require, character.only = T) # Libs
 
 p.scatter.plt <- function(x){ # Scatter plot of portfolio's securities
   
-  x <- x[,1 + 3 * seq(31, from = 0)] # Select securities columns
+  x <- x[,1+3*seq(ncol(x)%/%3,from=0)][,1:(ncol(x)%/%3)]
   
   d <- NULL # Empty variable to contain values
   
   for (n in 1:ncol(x)){ s <- x[,n] # For each security in data frame
-    
+  
     # Clean data to reduce NA and calculate return for ownership period  
     j <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
     
     d <- rbind.data.frame(d, cbind(sd(j)*1000, (exp(sum(j))-1)*100)) } # Join
-    
+  
   # Plot
   ggplot(d, mapping = aes(x = d[,1], y = d[,2])) + geom_point() +
     geom_text_repel(aes(label = colnames(x))) + theme_minimal() +
