3ae46f7d08d165df06f6f6a992b2a0a69da2d9d9
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-02 11:13:50 +0500

    Update Portfolio Securities SD and Returns.R
    Fixed bug leading to incorrect display of tickers

--- Scripts/Portfolio Measures/Portfolio Securities SD and Returns.R
+++ Scripts/Portfolio Measures/Portfolio Securities SD and Returns.R
@@ -1,8 +1,8 @@
 p.sd.return <- function(x){ # Data Frame with Standard Deviations and Returns
   
-  tickers <- colnames(x[,1 + 3 * seq(31, from = 0)]) # Tickers
+  tickers <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)][,1:(ncol(x)%/%3)])
   
-  x <- x[,1 + 3 * seq(31, from = 0)] # Select securities columns
+  x <- x[,1 + 3*seq(ncol(x) %/% 3, from = 0)][,1:(ncol(x)%/%3)] # Securities
   
   d <- NULL # Empty variable to contain values
   
@@ -12,7 +12,7 @@ p.sd.return <- function(x){ # Data Frame with Standard Deviations and Returns
     j <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
     
     d <- rbind.data.frame(d, cbind(sd(j)*1000, (exp(sum(j))-1)*100)) } # Join
-  
+    
   rownames(d) <- tickers # Tickers
   colnames(d) <- c("Standard Deviation", "Return") # Column names
   
