15c552345c58b38d5efbbce0d83f03c982033604
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-02 13:00:06 +0500

    Update Scatter Plot -  Risk & Return (Beta).R
    Fixed bug leading to incorrect display of tickers

--- Plots/Scripts for plots/Scatter Plot -  Risk & Return (Beta).R
+++ Plots/Scripts for plots/Scatter Plot -  Risk & Return (Beta).R
@@ -3,7 +3,7 @@ lapply(c("quantmod","timeSeries","ggplot2","ggrepel", "rvest"),
 
 p.scatter.plt.beta <- function(x){ # Portfolio Securities' Risk & Return (Beta) 
   
-  x <- x[,1 + 3 * seq(31, from = 0)] # Select securities columns
+  x <- x[,1+3*seq(ncol(x)%/%3,from=0)][,1:(ncol(x)%/%3)] # Securities
   
   d <- NULL # Empty variable to contain values
   
@@ -23,29 +23,29 @@ p.scatter.plt.beta <- function(x){ # Portfolio Securities' Risk & Return (Beta)
     
     if (is.na(b)){ l <- c(c, "^GSPC") # 
     
-      b <- NULL #
+    b <- NULL #
+    
+    for (m in l){ b <- cbind(b, getSymbols(m, from=as.Date(Sys.Date())-365*5,
+                                           to=Sys.Date(), src="yahoo",
+                                           auto.assign=F)[,4])}
+    
+      b <- b[apply(b, 1, function(x) all(!is.na(x))),] # Get rid of NA
       
-      for (m in l){ b <- cbind(b, getSymbols(m, from=as.Date(Sys.Date())-365*5,
-                                             to=Sys.Date(), src="yahoo",
-                                             auto.assign=F)[,4])}
+      b = diff(log(as.timeSeries(b)))[-1,] # Calculate Returns
       
-        b <- b[apply(b, 1, function(x) all(!is.na(x))),] # Get rid of NA
-        
-        b = diff(log(as.timeSeries(b)))[-1,] # Calculate Returns
-        
-        b <- apply(b[,1], 2, function(col) ((lm((col) ~
-                                                  b[,2]))$coefficients[2])) }
-        
+      b <- apply(b[,1], 2, function(col) ((lm((col) ~
+                                                b[,2]))$coefficients[2])) }
+      
     # Clean data to reduce NA and calculate return for ownership period  
     j <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
     
     d <- rbind.data.frame(d, cbind(b, (exp(sum(j)) - 1) * 100)) } # Join
     
   # Plot
-  ggplot(d, mapping = aes(x=d[,1], y=d[,2])) + geom_point() +
-    theme_minimal() + geom_text_repel(aes(label=colnames(x))) +
+  ggplot(d, mapping = aes(x=d[,1], y=d[,2])) + geom_point() + theme_minimal() +
+    geom_text_repel(aes(label=colnames(x))) +
     theme(plot.title = element_text(hjust = .5)) +
-    labs(title = "Performance of Portfolio Securities",
-         x = "Risk (Beta)", y = "Return (%)")
+    labs(title = "Performance of Portfolio Securities", x = "Risk (Beta)",
+         y = "Return (%)")
 }
 p.scatter.plt.beta(df_portfolio) # Test
