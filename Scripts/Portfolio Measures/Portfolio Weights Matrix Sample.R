b4ce0e06776682e51a1e85919b017194bda1ae19
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-02 11:31:36 +0500

    Update Portfolio Weights Matrix Sample.R
    Fixed bug leading to incorrect display of tickers

--- Scripts/Portfolio Measures/Portfolio Weights Matrix Sample.R
+++ Scripts/Portfolio Measures/Portfolio Weights Matrix Sample.R
@@ -1,23 +1,23 @@
 p.weights.mix.calculator <- function(x){ # Matrix of portfolio weights
   
-  a.names <- colnames(x[,1 + 3 * seq(31, from = 0)]) # Assign tickers
+  tickers <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)][,1:(ncol(x)%/%3)])
   
   # Take values of last observations
-  p.weights <- x[,3+3*seq(31,from=0)][nrow(x),]/as.numeric(x[nrow(x),ncol(x)])
+  w <- x[,3*seq(ncol(x)%/%3,from=1)][nrow(x),]/as.numeric(x[nrow(x),ncol(x)])
   
   x <- x[,3 + 3 * seq(31, from = 0)] # Assign total sum as main data frame
   
-  colnames(x) <- a.names # Assign tickers to this data frame
+  colnames(x) <- tickers # Assign tickers to this data frame
   
   # Create a matrix to store the products of weights
-  weight_product_matrix <- matrix(0,nrow=length(a.names),ncol=length(a.names))
+  weight_product_matrix <- matrix(0,nrow=length(tickers),ncol=length(tickers))
   
   # Fill the matrix with weight products
-  for (i in 1:length(a.names)) { for (j in 1:length(a.names)) {
-    weight_product_matrix[i, j] <- p.weights[i] * p.weights[j] } }
+  for (i in 1:length(tickers)) { for (j in 1:length(tickers)) {
+      weight_product_matrix[i, j] <- w[i] * w[j] } }
   
-  rownames(weight_product_matrix) <- a.names # Set row and column names
-  colnames(weight_product_matrix) <- a.names
+  rownames(weight_product_matrix) <- tickers # Set row and column names
+  colnames(weight_product_matrix) <- tickers
   
   return(weight_product_matrix) # Print the matrix
 }
