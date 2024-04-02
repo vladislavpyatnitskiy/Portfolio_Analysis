dc955618ce35cf85d5562fa2489a3b010673e234
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-02 10:26:50 +0500

    Update Dividend Data Frame without zeros.R
    Fixed bug leading to incorrect display of tickers

--- Scripts/Portfolio Dividends/Dividend Data Frame without zeros.R
+++ Scripts/Portfolio Dividends/Dividend Data Frame without zeros.R
@@ -1,6 +1,6 @@
 p.df.dividend.no.zeros <- function(x){ # Data Frame without zeros in total sum
   
-  x <- x[rowSums(x[,3 + 3 * seq(31, from = 0)]) > 0,] # Only > 0
+  x <- x[rowSums(x[,3 * seq(ncol(x) %/% 3, from = 1)]) > 0,] # Only > 0
   
   x # Display
 }
