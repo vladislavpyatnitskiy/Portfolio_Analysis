7e76dd8375de046d47270121913228a5b3a0de96
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-04 10:54:37 +0500

    Update Stacked Barplot (Sector).R
    Fixed bug leading to incorrect display of tickers

--- Plots/Scripts for plots/Stacked Barplot/Stacked Barplot (Sector).R
+++ Plots/Scripts for plots/Stacked Barplot/Stacked Barplot (Sector).R
@@ -2,10 +2,10 @@ lapply(c("ggplot2", "tidyverse", "rvest"), require, character.only = T) # Libs
 
 p.bar.plt.stack.sector <- function(x, portion=F){ # Stacked Bar Plot by Sectors
   
-  f.df <- x[,3 + 3 * seq(31, from = 0)] # Columns with total sum
-
+  f.df <- x[,3 * seq(ncol(x) %/% 3, from = 1)] # Columns with total sum
+  
   # Take column names with prices to put instead total sum column names
-  colnames(f.df) <- colnames(x[,1 + 3 * seq(31, from = 0)])  
+  colnames(f.df) <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)])[-(ncol(x)%/%3+1)]
   
   rwnms <- rownames(f.df) # Take dates from index column
   
@@ -17,10 +17,9 @@ p.bar.plt.stack.sector <- function(x, portion=F){ # Stacked Bar Plot by Sectors
   
   p.df <- NULL # Define variable to contain values
   
-  for (n in 2:ncol(f.df)){ s <- f.df[,n] # Loop to make monthly data
-  
-    # Convert daily data to monthly
-    v <- tapply(s, format(as.Date(f.df[,1]), "%Y-%m"), median)
+  for (n in 2:ncol(f.df)){ # Convert daily data to monthly
+    
+    v <- tapply(f.df[,n], format(as.Date(f.df[,1]), "%Y-%m"), median)
     
     rwmns_ds <- rownames(v) # Take dates from index column
     
@@ -46,7 +45,7 @@ p.bar.plt.stack.sector <- function(x, portion=F){ # Stacked Bar Plot by Sectors
   y <- NULL # Create list
   
   for (n in 1:length(rownames(p.df))){ s <- rownames(p.df)[n] 
-  
+    
     p <- sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s", s, s)
     
     page.p <- read_html(p) # Read HTML & extract necessary info
@@ -56,7 +55,7 @@ p.bar.plt.stack.sector <- function(x, portion=F){ # Stacked Bar Plot by Sectors
     h <- tab %>% html_nodes('p') %>% html_nodes('span') %>% html_text()
     
     y <- rbind(y, h[grep("Sector", h) + 1]) } # Add to list
-  
+    
   colnames(y) <- "Sector" # 
   rownames(y) <- rownames(p.df) # Assign tickers
   
@@ -65,7 +64,7 @@ p.bar.plt.stack.sector <- function(x, portion=F){ # Stacked Bar Plot by Sectors
   l <- NULL # Create time series with sector data
   
   for (n in 1:length(colnames(p.df))){ s <- as.data.frame(p.df[,n])
-  
+    
     pie.df <- data.frame(y, s) # Join data
     
     colnames(pie.df)[2] <- "Prices" # Assign column names
