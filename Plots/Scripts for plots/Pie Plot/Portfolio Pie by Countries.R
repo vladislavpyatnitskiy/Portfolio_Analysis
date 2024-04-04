9849d2238da5b350cbc9d2c3c067cb738fc729e6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-04 12:19:20 +0500

    Update Portfolio Pie by Countries.R
    Fixed bug leading to incorrect display of tickers

--- Plots/Scripts for plots/Pie Plot/Portfolio Pie by Countries.R
+++ Plots/Scripts for plots/Pie Plot/Portfolio Pie by Countries.R
@@ -2,7 +2,7 @@ library("rvest") # Library
 
 p.pie.plt.country <- function(x){ # Portfolio Pie Plot by Countries
   
-  tickers <- colnames(x[,1 + 3 * seq(31, from = 0)]) # tickers
+  tickers <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)])[-(ncol(x)%/%3+1)]
   
   df <- NULL
   
@@ -19,10 +19,12 @@ p.pie.plt.country <- function(x){ # Portfolio Pie Plot by Countries
     l <- strsplit(toString(c), "<br>")[[1]][length(strsplit(toString(c),
                                                             "<br>")[[1]])-4]
     df <- rbind.data.frame(df, l) } # Data Frame 
-  
+    
   # Calculate Weights for each security
-  y <- round(as.data.frame(t(as.data.frame(x[,3+3*seq(31,from=0)][nrow(x),]))/
-                             x[nrow(x),][,ncol(x)]),2)*100
+  y <- round(as.data.frame(t(as.data.frame(x[,3*seq(ncol(x)%/%3,
+                                                    from=1)][nrow(x),]/
+                                             as.numeric(x[nrow(x),
+                                                          ncol(x)])))),2)*100
   rownames(y) <- tickers
   rownames(df) <- tickers # Tickers
   
@@ -32,15 +34,14 @@ p.pie.plt.country <- function(x){ # Portfolio Pie Plot by Countries
   
   df <- aggregate(Portion ~ Country, data=df, sum) # Conditional sum
   
-  colors37 = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d",
-               "#5a51dc","#d49f36","#552095","#507f2d","#db37aa","#84b67c",
-               "#a06fda","#df462a","#5b83db","#c76c2d","#4f49a3","#82702d",
-               "#dd6bbb","#334c22","#d83979","#55baad","#dc4555","#62aad3",
-               "#8c3025","#417d61","#862977","#bba672","#403367","#da8a6d",
-               "#a79cd4","#71482c","#c689d0","#6b2940","#d593a7","#895c8b",
-               "#bd5975")
+  C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
+        "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
+        "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
+        "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
+        "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
+        "#895c8b","#bd5975")
   
-  pie(df[,2], labels = c(sprintf("%s %s%%", df[,1], df[,2])), col = colors37,
-      main = "Portfolio by Countries", radius = 1.1) # Plot
+  pie(df[,2], labels=c(sprintf("%s %s%%", df[,1], df[,2])), col=C, radius=1.7,
+      main = "Portfolio Securities by Countries") # Plot
 }
 p.pie.plt.country(df_portfolio) # Test
