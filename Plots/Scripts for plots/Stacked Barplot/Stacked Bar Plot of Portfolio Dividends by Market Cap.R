b09fc9dff804f77a60e2d9dd93fe3bee41c22d3a
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-04 11:18:20 +0500

    Update Stacked Bar Plot of Portfolio Dividends by Market Cap.R
    Fixed bug leading to incorrect display of tickers

--- Plots/Scripts for plots/Stacked Barplot/Stacked Bar Plot of Portfolio Dividends by Market Cap.R
+++ Plots/Scripts for plots/Stacked Barplot/Stacked Bar Plot of Portfolio Dividends by Market Cap.R
@@ -3,10 +3,10 @@ lapply(c("ggplot2", "tidyverse", "rvest"), require, character.only = T) # Libs
 # Stacked Bar Plot of Portfolio Securities Dividends by Market Cap
 p.bar.plt.stack.dividend.marketcap <- function(x, portion = F){ 
   
-  f.df <- x[,3 + 3 * seq(31, from = 0)] # Total sum of dividends for each stock
+  f.df <- x[,3 * seq(ncol(x) %/% 3, from = 1)] # Columns with total sum
   
   # Take column names with prices to put instead total sum column names
-  colnames(f.df) <- colnames(x[,1 + 3 * seq(31, from = 0)])  
+  colnames(f.df) <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)])[-(ncol(x)%/%3+1)]
   
   f.df <- f.df[rowSums(f.df) > 0,][,colSums(f.df) > 0] # Only > 0
   
@@ -20,9 +20,9 @@ p.bar.plt.stack.dividend.marketcap <- function(x, portion = F){
   
   p.df <- NULL # Define variable to contain values
   
-  for (n in 2:ncol(f.df)){ s <- f.df[,n] # Loop to make monthly data
-  
-    v <- tapply(s, format(as.Date(f.df[,1]), "%Y-%m"), sum) # Daily to monthly
+  for (n in 2:ncol(f.df)){ # Convert daily data to monthly
+    
+    v <- tapply(f.df[,n], format(as.Date(f.df[,1]), "%Y-%m"), sum)
     
     rwmns_ds <- rownames(v) # Take dates from index column
     
@@ -34,7 +34,7 @@ p.bar.plt.stack.dividend.marketcap <- function(x, portion = F){
     
     # If defined empty variable is still empty # Put new dataset there
     if (is.null(p.df)){ p.df<-v } else { p.df <- merge(x=p.df,y=v,by="Date")} }
-  
+    
   p.df <- as.data.frame(p.df) # Convert to data frame format
   
   colnames(p.df) <- colnames(f.df) # Give column names
@@ -48,7 +48,7 @@ p.bar.plt.stack.dividend.marketcap <- function(x, portion = F){
   y <- NULL # Create list
   
   for (n in 1:length(rownames(p.df))){ v <- rownames(p.df)[n]  # Subset ticker
-    
+  
     p <- sprintf("https://finance.yahoo.com/quote/%s/key-statistics?p=%s",v,v)
     
     page.p <- read_html(p) # Read HTML & extract necessary info
@@ -94,7 +94,7 @@ p.bar.plt.stack.dividend.marketcap <- function(x, portion = F){
     pie.df <- aggregate(Prices ~ Marketcap, data=pie.df, sum) # Conditional sum
     
     if (is.null(l)){ l <- pie.df } else { l<-merge(l,pie.df,by="Marketcap") } }
-  
+    
   rownames(l) <- l[,1] # Assign Market Cap info as row names
   
   l <- l[,-1] # Reduce Market Cap info as it is now in row names
@@ -108,13 +108,12 @@ p.bar.plt.stack.dividend.marketcap <- function(x, portion = F){
   colnames(i)[colnames(i) == colnames(i[1])] <- 'Date' # Rename again
   
   # Colour set for plot
-  colors37 = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d",
-               "#5a51dc","#d49f36","#552095","#507f2d","#db37aa","#84b67c",
-               "#a06fda","#df462a","#5b83db","#c76c2d","#4f49a3","#82702d",
-               "#dd6bbb","#334c22","#d83979","#55baad","#dc4555","#62aad3",
-               "#8c3025","#417d61","#862977","#bba672","#403367","#da8a6d",
-               "#a79cd4","#71482c","#c689d0","#6b2940","#d593a7","#895c8b",
-               "#bd5975")
+  C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
+        "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
+        "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
+        "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
+        "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
+        "#895c8b","#bd5975")
   
   # Convert for better read by ggplot
   i <- i %>% pivot_longer(cols=-Date, names_to="Stock", values_to="Quantity")
@@ -125,7 +124,7 @@ p.bar.plt.stack.dividend.marketcap <- function(x, portion = F){
       geom_bar(position = "fill", stat = "identity") + 
       labs(title="Stacked Bar Plot of Portfolio Dividends by Market Cap",
            x = "Months", y = "Stakes (%)", fill = "Market Cap Level") +
-      scale_fill_manual(values = colors37)   
+      scale_fill_manual(values = C)   
     
   } else { # Generate plot showing amount of dividends for each month
     
@@ -133,6 +132,6 @@ p.bar.plt.stack.dividend.marketcap <- function(x, portion = F){
       geom_bar(position = "stack", stat = "identity") + 
       labs(title="Stacked Bar Plot of Portfolio Dividends by Market Cap",
            x = "Months", y = "Amount in $US", fill = "Market Cap Level") +
-      scale_fill_manual(values = colors37) }
+      scale_fill_manual(values = C) }
 }
 p.bar.plt.stack.dividend.marketcap(df_portfolio_dividend, portion = T) # Test
