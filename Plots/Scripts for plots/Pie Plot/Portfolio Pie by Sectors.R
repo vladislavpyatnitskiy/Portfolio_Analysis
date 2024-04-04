13a54dfbbf514627c0e4094f37783ae92f093c39
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-04 12:38:26 +0500

    Update Portfolio Pie by Sectors.R
    Fixed bug leading to incorrect display of tickers

--- Plots/Scripts for plots/Pie Plot/Portfolio Pie by Sectors.R
+++ Plots/Scripts for plots/Pie Plot/Portfolio Pie by Sectors.R
@@ -2,39 +2,37 @@ library("rvest")
 
 p.pie.plt.sector <- function(x, main = NULL, sub = NULL){ 
   
-  tickers <- colnames((x[,1 + 3 * seq(31, from = 0)]))  # Subset tickers
+  tickers <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)])[-(ncol(x)%/%3+1)]
   
-  pct <- as.data.frame(x[,3+3*seq(31,from=0)])/x[,ncol(x)] # Calculate portions
+  pct <- as.data.frame(x[,3 * seq(ncol(x) %/% 3, from = 1)]) / x[,ncol(x)]
   
   colnames(pct) <- tickers # Assign tickers
   
   pct <- t(round(pct[nrow(pct),] * 100)) # Transform last period into percents
   
   # Add colour range
-  colors37 = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d",
-               "#5a51dc","#d49f36","#552095","#507f2d","#db37aa","#84b67c",
-               "#a06fda","#df462a","#5b83db","#c76c2d","#4f49a3","#82702d",
-               "#dd6bbb","#334c22","#d83979","#55baad","#dc4555","#62aad3",
-               "#8c3025","#417d61","#862977","#bba672","#403367","#da8a6d",
-               "#a79cd4","#71482c","#c689d0","#6b2940","#d593a7","#895c8b",
-               "#bd5975")
+  C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
+        "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
+        "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
+        "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
+        "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
+        "#895c8b","#bd5975")
   
   colnames(pct) <- "Portion" # Assign column name
   
   l <- NULL # Create list
   
-  for (n in 1:length(tickers)){ s <- tickers[n] # For each security find sector
+  for (n in 1:length(tickers)){ # Read HTML & extract necessary info
   
-    p <- sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s", s, s)
+    p <- read_html(sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s",
+                           tickers[n], tickers[n])) 
     
-    page.p <- read_html(p) # Read HTML & extract necessary info
+    price.yahoo1 <- p %>% html_nodes('div') %>% .[[1]] -> tab
     
-    price.yahoo1 <- page.p %>% html_nodes('div') %>% .[[1]] -> tab11
+    y <- tab %>% html_nodes('p') %>% html_nodes('span') %>% html_text()
     
-    y <- tab11 %>% html_nodes('p') %>% html_nodes('span') %>% html_text()
+    l <- rbind(l, y[2]) } # Add to list
     
-    l <- rbind(l, y[2])} # Add to list
-  
   colnames(l) <- "Sector" # Assign column name
   rownames(l) <- tickers # Assign tickers
   
@@ -43,8 +41,7 @@ p.pie.plt.sector <- function(x, main = NULL, sub = NULL){
   pie.df <- data.frame(l, pct) # Form data frame
   pie.df <- aggregate(Portion ~ Sector, data=pie.df, sum) # Conditional sum
   
-  # Pie Chart
-  pie(pie.df[,2],labels=c(sprintf("%s %s%%",pie.df[,1],pie.df[,2])),
-      col=colors37, main=main,sub=sub, radius=1.5) 
+  pie(pie.df[,2], labels=c(sprintf("%s %s%%", pie.df[,1], pie.df[,2])), col=C,
+      main=main, sub=sub, radius=1.5) # Pie Chart
 }
 p.pie.plt.sector(df_portfolio, main = "Portfolio by Sectors") # Test
