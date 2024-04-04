a2f8df2de48fe35c6fa3b08a67f4ffdd3c123823
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-04 10:42:15 +0500

    Update Stacked Barplot.R
    Fixed bug leading to incorrect display of tickers

--- Plots/Scripts for plots/Stacked Barplot/Stacked Barplot.R
+++ Plots/Scripts for plots/Stacked Barplot/Stacked Barplot.R
@@ -2,10 +2,10 @@ lapply(c("ggplot2", "tidyverse"), require, character.only = T) # Libraries
 
 p.bar.plt.stack <- function(x, portion = T){ # Stacked Bar Plot
   
-  f.df <- x[,3 + 3 * seq(31, from = 0)] # Subset columns with total sum
-
+  f.df <- x[,3 * seq(ncol(x) %/% 3, from = 1)] # Subset columns with total sum
+  
   # Take column names with prices to put instead total sum column names
-  colnames(f.df) <- colnames(x[,1 + 3 * seq(31, from = 0)])  
+  colnames(f.df) <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)])[-(ncol(x)%/%3+1)]
   
   rwnms <- rownames(f.df) # Take dates from index column
   
@@ -17,10 +17,9 @@ p.bar.plt.stack <- function(x, portion = T){ # Stacked Bar Plot
   
   p.df <- NULL # Define variable to contain values
   
-  for (n in 2:ncol(f.df)){ s <- f.df[,n] # Loop to make monthly data
+  for (n in 2:ncol(f.df)){ # Convert daily data to monthly
   
-    # Convert daily data to monthly
-    v <- tapply(s, format(as.Date(f.df[,1]), "%Y-%m"), median)
+    v <- tapply(f.df[,n], format(as.Date(f.df[,1]), "%Y-%m"), median)
     
     rwmns_ds <- rownames(v) # Take dates from index column
     
@@ -40,13 +39,12 @@ p.bar.plt.stack <- function(x, portion = T){ # Stacked Bar Plot
   colnames(p.df)[colnames(p.df) == colnames(p.df[1])] <- 'Date' # Rename again
   
   # Colour set for plot
-  colors37 = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d",
-               "#5a51dc","#d49f36","#552095","#507f2d","#db37aa","#84b67c",
-               "#a06fda","#df462a","#5b83db","#c76c2d","#4f49a3","#82702d",
-               "#dd6bbb","#334c22","#d83979","#55baad","#dc4555","#62aad3",
-               "#8c3025","#417d61","#862977","#bba672","#403367","#da8a6d",
-               "#a79cd4","#71482c","#c689d0","#6b2940","#d593a7","#895c8b",
-               "#bd5975")
+  C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
+        "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
+        "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
+        "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
+        "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
+        "#895c8b","#bd5975")
   
   # Convert for better read by ggplot
   p.df<-p.df %>% pivot_longer(cols=-Date,names_to="Stock",values_to="Quantity")
@@ -55,16 +53,14 @@ p.bar.plt.stack <- function(x, portion = T){ # Stacked Bar Plot
     
     ggplot(p.df, aes(x = Date, y = Quantity, fill = Stock)) + theme_minimal() +
       geom_bar(position = "fill", stat = "identity") + 
-      labs(title = "Stacked Bar Plot of Portfolio Securities",
-           x = "Months", y = "Stakes (%)", fill = "Securities") +
-      scale_fill_manual(values = colors37)   
+      labs(title = "Stacked Bar Plot of Portfolio Securities", x = "Months",
+           y="Stakes (%)", fill="Securities") + scale_fill_manual(values=C)   
     
   } else { # Generate plot showing amount of securities for each month
     
     ggplot(p.df, aes(x = Date, y = Quantity, fill = Stock)) + theme_minimal() +
       geom_bar(position = "stack", stat = "identity") + 
-      labs(title = "Stacked Bar Plot of Portfolio Securities",
-           x = "Months", y = "Amount in $US", fill = "Securities") +
-      scale_fill_manual(values = colors37) }
+      labs(title = "Stacked Bar Plot of Portfolio Securities", x = "Months",
+           y="Amount in $US", fill="Securities") + scale_fill_manual(values=C)}
 }  
 p.bar.plt.stack(df_portfolio, portion = F) # Test
