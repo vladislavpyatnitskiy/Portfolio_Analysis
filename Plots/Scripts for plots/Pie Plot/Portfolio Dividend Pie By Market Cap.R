7e9d1b3bec5b2a5148ab0fa502611aeaeb45730f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-05 11:51:20 +0500

    Update Portfolio Dividend Pie By Market Cap.R
    Fixed bug leading to incorrect display of tickers

--- Plots/Scripts for plots/Pie Plot/Portfolio Dividend Pie By Market Cap.R
+++ Plots/Scripts for plots/Pie Plot/Portfolio Dividend Pie By Market Cap.R
@@ -2,11 +2,11 @@ lapply(c("rvest", "plotly", "timeSeries"), require, character.only = T) # Libs
 
 p.pie.plt.dividend.marketcap <- function(x){ # Plot of Portfolio Dividends
   
-  s.names <- colnames(x[,1 + 3 * seq(31, from = 0)]) # Subset tickers
+  tickers <- colnames(x[,1+3*seq(ncol(x) %/% 3,from=0)])[-(ncol(x) %/% 3+1)]
   
-  x <- cumsum(x[,3 + 3 * seq(31, from = 0)]) # Calculate Cumulative Divs
+  x <- cumsum(x[,3 * seq(ncol(x) %/% 3, from = 1)]) # Calculate Cumulative Divs
   
-  colnames(x) <- s.names # Assign tickers
+  colnames(x) <- tickers # Assign tickers
   
   x <- cbind(x, as.timeSeries(rowSums(x, na.rm = T))) # Join with Total Sum
   
@@ -14,13 +14,13 @@ p.pie.plt.dividend.marketcap <- function(x){ # Plot of Portfolio Dividends
   
   x <- x[,colSums(x) !=0] # Reduce column without dividends
   
-  s.names <- colnames(x) # Assign tickers of securities without dividends
+  tickers <- colnames(x) # Assign tickers of securities without dividends
   
   x <- as.numeric(x[nrow(x),]) / as.numeric(x[nrow(x),ncol(x)]) # Find %
   
   x <- x[-length(x)] # Reduce column with total sum (100%)
   
-  tickers <- s.names[-length(s.names)] # Reduce name with total sum (100%)
+  tickers <- tickers[-length(tickers)] # Reduce name with total sum (100%)
   
   v <- data.frame(c(round(x * 100, 2))) # Data Frame with tickers & %
   
@@ -69,7 +69,8 @@ p.pie.plt.dividend.marketcap <- function(x){ # Plot of Portfolio Dividends
   
   plot_ly(df, labels = ~df[,1], values = ~df[,2] , type = 'pie',
           textposition = 'outside',textinfo = 'percent') %>%
-    layout(title = "Portfolio Dividends by Market Capitalisation",
+    layout(title="Dividends of Portfolio Securities by Market Capitalisation",
+           margin = list(l = 20, r = 20, t = 120),
            xaxis = list(showgrid = F, zeroline = F, showticklabels = F),
            yaxis = list(showgrid = F, zeroline = F, showticklabels = F))
 }
