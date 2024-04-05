32be0beb2c56d922bfa619542beba6a935bd3f2b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-04-05 11:45:24 +0500

    Update Portfolio Dividend Pie By Sectors.R
    Fixed bug leading to incorrect display of tickers

--- Plots/Scripts for plots/Pie Plot/Portfolio Dividend Pie By Sectors.R
+++ Plots/Scripts for plots/Pie Plot/Portfolio Dividend Pie By Sectors.R
@@ -1,12 +1,12 @@
-lapply(c("rvest", "plotly"), require, character.only = T) # Libs
+lapply(c("rvest", "plotly", "timeSeries"), require, character.only = T) # Libs
 
 p.pie.plt.dividend.sector <- function(x){ # Plot of Portfolio Dividends
   
-  s.names <- colnames(x[,1 + 3 * seq(31, from = 0)]) # Subset tickers
+  tickers <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)])[-(ncol(x)%/%3+1)]
   
-  x <- cumsum(x[,3 + 3 * seq(31, from = 0)]) # Calculate Cumulative Divs
+  x <- cumsum(x[,3 * seq(ncol(x) %/% 3, from = 1)]) # Calculate Cumulative Divs
   
-  colnames(x) <- s.names # Assign tickers
+  colnames(x) <- tickers # Assign tickers
   
   x <- cbind(x, as.timeSeries(rowSums(x, na.rm = T))) # Join with Total Sum
   
@@ -14,13 +14,13 @@ p.pie.plt.dividend.sector <- function(x){ # Plot of Portfolio Dividends
   
   x <- x[,colSums(x) !=0] # Reduce column without dividends
   
-  s.names <- colnames(x) # Assign tickers of securities without dividends
+  tickers <- colnames(x) # Assign tickers of securities without dividends
   
   x <- as.numeric(x[nrow(x),]) / as.numeric(x[nrow(x),ncol(x)]) # Find %
   
   x <- x[-length(x)] # Reduce column with total sum (100%)
   
-  tickers <- s.names[-length(s.names)] # Reduce name with total sum (100%)
+  tickers <- tickers[-length(tickers)] # Reduce name with total sum (100%)
   
   v <- data.frame(c(round(x * 100, 2))) # Data Frame with tickers & %
   
@@ -30,27 +30,27 @@ p.pie.plt.dividend.sector <- function(x){ # Plot of Portfolio Dividends
   
   for (n in 1:length(tickers)){ s <- tickers[n] # For each security find sector
   
-    p <- sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s", s, s)
+    p <- read_html(sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s",
+                           s, s)) # Read HTML & extract necessary info
     
-    page.p <- read_html(p) # Read HTML & extract necessary info
-    
-    price.yahoo1 <- page.p %>% html_nodes('div') %>% .[[1]] -> tab
+    price.yahoo1 <- p %>% html_nodes('div') %>% .[[1]] -> tab
     
     y <- tab %>% html_nodes('p') %>% html_nodes('span') %>% html_text()
     
-    l <- rbind(l, y[2])} # Add to list
-  
+    l <- rbind(l, y[2]) } # Add to list
+    
   rownames(l) <- tickers # Assign tickers
   
   df <- data.frame(l, v) # Join Sectors and portions info
   
   colnames(df) <- c("Sector", "Portion") # Assign column name
   
-  df <- aggregate(Portion ~ Sector, data=df, sum) # Conditional sum
+  df <- aggregate(Portion ~ Sector, data = df, sum) # Conditional sum
   
   plot_ly(df, labels = ~df[,1], values = ~df[,2] , type = 'pie',
           textposition = 'outside',textinfo = 'percent') %>%
-    layout(title = "Portfolio Dividends by Sectors",
+    layout(title = "Dividends of Portfolio Securities by Sectors",
+           margin = list(l = 20, r = 20, t = 120),
            xaxis = list(showgrid = F, zeroline = F, showticklabels = F),
            yaxis = list(showgrid = F, zeroline = F, showticklabels = F))
 }
