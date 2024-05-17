eb4c85bef56b64c65d5eaf6d257fa357ceb4a758
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-05-17 10:35:58 +0500

    Update Portfolio Dividends Allocation Barplot.R
    Optimised script:
    
    • Renamed variables
    • Moved legend under plot
    • Reduced excessive lines

--- Plots/Scripts for plots/Portfolio Dividends Allocation Barplot.R
+++ Plots/Scripts for plots/Portfolio Dividends Allocation Barplot.R
@@ -26,24 +26,16 @@ p.bar.plt.weights.dividend <- function(x, sort = T, decreasing = T){
   
   if (isTRUE(sort)) { v <- sort(v, decreasing = decreasing) } # Sort
   
-  # Add colour range
-  colors37 = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d",
-               "#5a51dc","#d49f36","#552095","#507f2d","#db37aa","#84b67c",
-               "#a06fda","#df462a","#5b83db","#c76c2d","#4f49a3","#82702d",
-               "#dd6bbb","#334c22","#d83979","#55baad","#dc4555","#62aad3",
-               "#8c3025","#417d61","#862977","#bba672","#403367","#da8a6d",
-               "#a79cd4","#71482c","#c689d0","#6b2940","#d593a7","#895c8b",
-               "#bd5975")
+  C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
+        "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
+        "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
+        "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
+        "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
+        "#895c8b","#bd5975") # Add colour range
   
-  # Create barplot
-  bar.plt.script <- barplot(v,
-                            names.arg = names(v),
-                            horiz = F,
-                            col = colors37,
-                            main="Dividend Allocation of Portfolio Securities",
-                            ylab = "Percentage (%)",
-                            ylim = c(0, ceiling(max(v))),
-                            las = 2)
+  B <- barplot(v, names.arg = names(v), horiz = F, ylab = "Percentage (%)",
+               main="Dividend Allocation of Portfolio Securities", las = 2,
+               ylim = c(0, ceiling(max(v))), col = C) # Create Bar Plot
   
   p.seq <- seq(0, ceiling(max(v)), 5) # Y axis
   
@@ -52,23 +44,16 @@ p.bar.plt.weights.dividend <- function(x, sort = T, decreasing = T){
   par(mar = c(5, 4, 4, 4)) # Define borders of the plot to fit right y-axis
   
   # Add grey lines for fast visual percentage calculation
-  for (n in seq(0, ceiling(max(v)), 5)){ abline(h = n, col ="grey",lty = 3) }
+  for (n in seq(0, ceiling(max(v)), 5)){ abline(h = n, col ="grey", lty = 3) }
   
-  abline(v = bar.plt.script, col = "grey", lty = 3)
+  abline(v = B, col = "grey", lty = 3)
   abline(h = mean(v), col = "red", lwd = 3) # Mean percentage line
   abline(h = median(v), col = "green", lwd = 3) # Median percentage line
   
-  if (isTRUE(decreasing)){ # Box with legend with mean and median on Top Right
-    
-    legend("topright", legend=c((sprintf("Mean:    %s %%", round(mean(v), 3))),
-                                sprintf("Median: %s %%", round(median(v), 3))),
-           fill = c("red", "green"), cex = .75, bty = "n")
-    
-  } else { # Box with legend with mean and median on Top Left
-    
-    legend("topleft", legend=c((sprintf("Mean:    %s %%", mean(v))),
-                               sprintf("Median: %s %%", round(median(v),3))),
-           fill = c("red", "green"), cex = .75, bty = "n") }
+  legend(x = "bottom", inset = c(0, -.22), cex = .85, bty = "n", horiz = T,
+         legend = c((sprintf("Mean: %s %%", round(mean(v), 3))),
+                    sprintf("Median: %s %%", round(median(v), 3))),
+         col = c("red", "green"), xpd = T, pch = 15)
   
   box() # Box
 }
