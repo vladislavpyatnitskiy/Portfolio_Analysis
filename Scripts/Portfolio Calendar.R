c3da8fa8adbb82b5c06b784a966fe29303492b5b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-06-28 08:30:30 +0500

    Update Portfolio Calendar.R
    Updated and optimised script:
    
    • Reduced excessive lines
    • Renamed variables
    • Edited comments
    • Set up transformation from portfolio data frame, so no need to calculate returns first
    • Left blank cells with excess data instead of "XX" notations

--- Scripts/Portfolio Calendar.R
+++ Scripts/Portfolio Calendar.R
@@ -1,112 +1,113 @@
-p.calendar <- function(x){ # Table of Portfolio Returns
+library("timeSeries") # Library
+
+p.calendar <- function(x){ # Calendar of Portfolio Returns
   
-  f.df <- x   # Assign first log as 0
+  x <- x[,3*seq(ncol(x)%/%3,from=1)] # Subtract total values for each stock
   
-  r.rownames <- rownames(f.df) # Take dates from index column
+  x1 <- t(x) # Transpose data frame
   
-  r.rownames <- as.Date(r.rownames) # Make it in date format
+  colnames(x1) <- rownames(x) # Make dates as column names x1 and x
   
-  f.df <- data.frame(r.rownames, f.df) # Join it with main data set
+  r <- as.data.frame(0) # Define data frame with value zero
   
-  rownames(f.df) <- seq(nrow(f.df)) # Create sequence for index column
+  # Loop for portfolio log returns calculation
+  for (n in 2:ncol(x1)){ df2p <- x1[,(n-1):n] # x1 # Select two periods
   
-  p.df <- NULL # Define variable to contain values
-  
-  for (n in 2:ncol(f.df)){ s <- f.df[,n] # Loop to make monthly data
-  
-  # Convert daily data to monthly
-  v <- round(tapply(s, format(as.Date(f.df[,1]),"%Y-%m"), sum) ,4) * 100
-  
-  df.rownames <- rownames(v) # Take dates from index column
-  
-  v <- data.frame(df.rownames, v) # Join with new data set
-  
-  rownames(v) <- seq(nrow(v)) # Generate sequence for index column
-  
-  colnames(v)[colnames(v) == 'df.rownames'] <- 'Date' # Name column as Date
-  
-  # If defined empty variable is still empty # Put new dataset there
-  if (is.null(p.df)){ p.df<-v } else { p.df <- merge(x=p.df,y=v,by="Date")} }
-  
-  p.df <- as.data.frame(p.df) # Convert to data frame format
-  
-  colnames(p.df) <- colnames(f.df) # Give column names
-  
-  colnames(p.df)[colnames(p.df) == colnames(p.df[1])] <- 'Date'
-  
-  ticker <- sprintf("%s (%%)", colnames(p.df)[2])
-  
-  d.y <- as.data.frame(substr(p.df[,1],1,4))
-  d.m <- as.data.frame(substr(p.df[,1],6,7))
-  
-  p.df <- p.df[,-1]
-  
-  p.df <- data.frame(d.y, d.m, p.df)
-  
-  colnames(p.df) <- c("Year", "Month", ticker)
-  
-  #p.df
-  l <- NULL # Data Frame for Joined Year columns
-  
-  for (m in 1:length(unique(p.df[,1]))){ # 
+    s <- df2p[apply(df2p, 1, function(row) all(row !=0 )),] # Remove zeros & NA
     
-    l1 <- as.data.frame(sort(unique(p.df[,2]))) # Sort months ascendingly
+    r <- rbind(r, log(colSums(s)[2]/colSums(s)[1])) } # Add value to data frame
+  
+  colnames(r) <- "Returns" # Give name to column
+  rownames(r) <- rownames(x) # Return dates to index
+  
+  r <- as.timeSeries(r) # Make it time series
+  
+  r <- data.frame(rownames(r), r) # Subtract dates and Join it with data set
+  
+  rownames(r) <- seq(nrow(r)) # Create sequence for index column
+  
+  D <- NULL # Define variable to contain values
+  
+  for (n in 2:ncol(r)){ # Loop to convert daily data to monthly
     
-    l2 <- p.df[p.df[,1] == unique(p.df[,1])[m],] # First unique year
+    v <- round(tapply(r[,n], format(as.Date(r[,1]), "%Y-%m"), sum), 4) * 100
     
-    colnames(l1) <- "Month" # Give column name to Data Frame of months
+    v <- data.frame(rownames(v), v) # Take dates from index column and join
     
-    l3 <- merge(l2, l1, by = "Month", all = T) # Merge months Data Frames
+    rownames(v) <- seq(nrow(v)) # Generate sequence for index column
     
-    l3 <- l3[,c("Year","Month",colnames(p.df)[3])] # Column names to DF
+    colnames(v)[1] <- 'Date' # Name columns as Date and ticker name
+    colnames(v)[2] <- colnames(x)[n]
     
-    if (isTRUE(any(is.na(l3)))){ # Give name to year observation with NA
+    # If defined empty variable is still empty # Put new dataset there
+    if (is.null(D)){ D <- v } else { D <- merge(x = D, y = v, by = "Date") } }
+    
+  ticker <- sprintf("%s (%%)", colnames(D)[2]) # Column name for return
+    
+  Y <- as.data.frame(substr(D[,1], 1, 4)) # Subtract Year Value
+  M <- as.data.frame(substr(D[,1], 6, 7)) # Subtract Month Value
+    
+  D <- D[,-1] # Reduce first column containing both year and month values
+    
+  D <- data.frame(Y, M, D) # Join data with Years, Months and Returns
+    
+  colnames(D) <- c("Year", "Month", ticker) # Columns with Year, Month & Return
+    
+  C <- NULL # Data Frame for Joined Year columns
+    
+  # Firstly, Sort months in an ascending way
+  for (m in 1:length(unique(D[,1]))){ l <- as.data.frame(sort(unique(D[,2]))) 
       
-      l3[is.na(l3[,1]),][,1] <- p.df[p.df[,1]==unique(p.df[,1])[m],][1,1]}
-    
-    l3 <- l3[,-1] # Delete Year Column 
-    
+    y <- D[D[,1] == unique(D[,1])[m],] # Subtract unique years
+      
+    colnames(l) <- "Month" # Give column name to Data Frame of months
+      
+    d <- merge(y, l, by = "Month", all = T) # Merge months Data Frames
+      
+    d <- d[,c("Year", "Month", colnames(D)[3])] # Column names to DF
+      
+    if (isTRUE(any(is.na(d)))){ # Give name to year observation with NA
+        
+      d[is.na(d[,1]),][,1] <- D[D[,1] == unique(D[,1])[m],][1,1] }
+      
+    d <- d[,-1] # Delete Year Column 
+      
     # Assign Year number name as column name of returns
-    colnames(l3)[2] <- p.df[p.df[,1] == unique(p.df[,1])[m],][1,1]
-    
-    # Data Frame with months
-    m1 <- data.frame(l3[,1], c("January", "February", "March", "April",
-                               "May", "June", "July", "August", "September",
-                               "October", "November", "December"))
-    
+    colnames(d)[2] <- D[D[,1] == unique(D[,1])[m],][1,1]
+      
+    m1 <- data.frame(d[,1], month.name) # Data Frame with months
+      
     colnames(m1) <- c("Month", "Months") # Column names for numbers and names
-    
-    l4 <- merge(m1, l3, by = "Month") # Join Months and returns by numbers
-    
+      
+    l4 <- merge(m1, d, by = "Month") # Join Months and returns by numbers
+      
     l4 <- l4[,-1] # Reduce column with number of months instead of names
-    
+      
     m2 <- l4[,1] # Assign month column to new variable
-    
+      
     l4 <- as.data.frame(l4[,-1]) # Reduce excessive month column
-    
+      
     rownames(l4) <- m2 # Months as row names and year as column name
-    colnames(l4) <- p.df[p.df[,1] == unique(p.df[,1])[m],][1,1] 
+    colnames(l4) <- D[D[,1] == unique(D[,1])[m],][1,1] 
+      
+    if (is.null(C)){ C <- l4 } else { C <- cbind(C, l4) } } # Join Years
     
-    if (is.null(l)){ l <- l4 } else { l <- cbind(l, l4) } } # Join
+  C$Median <- round(apply(C, 1, median, na.rm = T), 2) # Monthly Median
+  C$Mean <- round(apply(C[,1:(ncol(C)-1)], 1, mean, na.rm=T), 2) # Monthly Mean
   
-  # Median and Mean for each month
-  l$Median <- round(apply(l, 1, median, na.rm = T), 2)
-  l$Mean <- round(apply(l[,1:(ncol(l)-1)], 1, mean, na.rm = T), 2)
+  g <- list(c(0, 1), c(0, 1, 2), list(sum, median, mean)) # List with values
   
-  # Sum, Median and Mean for each year
-  l[nrow(l) + 1,] = round(apply(l, 2, sum, na.rm = T), 2)
-  l[nrow(l) + 1,] = round(apply(l[1:(nrow(l)-1),], 2, median, na.rm = T), 2)
-  l[nrow(l) + 1,] = round(apply(l[1:(nrow(l)-2),], 2, mean, na.rm = T), 2)
+  for (n in 1:length(g[[3]])){ # Calculate Sum, Median and Mean for each year
+    
+    C[nrow(C) + 1,] = round(apply(C[1:(nrow(C) - g[[2]][n]),], 2, g[[3]][[n]],
+                                  na.rm = T), 2) }
   
-  rownames(l)[(nrow(l) - 2):nrow(l)] <- c("Sum", "Median", "Mean") # Names
+  for (n in 1:length(g[[3]])){ for (m in 1:length(g[[1]])){ # Cut unnecessary
+    
+      C[nrow(C) - g[[2]][n], ncol(C) - g[[1]][m]] <- "" } } # Values
   
-  l[nrow(l), ncol(l)] <- "XX"
-  l[nrow(l) - 1, ncol(l)] <- "XX"
-  l[nrow(l), ncol(l) - 1] <- "XX"
-  l[nrow(l) - 1, ncol(l)-1] <- "XX"
-  l[nrow(l) - 2, ncol(l) - 1] <- "XX"
-  l[nrow(l) - 2, ncol(l)] <- "XX"
+  rownames(C)[(nrow(C) - 2):nrow(C)] <- c("Sum", "Median", "Mean") # Names
   
-  l
+  C # Display
 }
-p.calendar(returns_df) # Test
+p.calendar(df_portfolio) # Test
