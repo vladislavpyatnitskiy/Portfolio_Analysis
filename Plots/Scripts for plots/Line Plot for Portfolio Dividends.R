d3b9bbf2a2ac3266eabfcd69b0dad349b0c4a1c1
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-07-10 11:51:43 +0500

    Update Line Plot for Portfolio Dividends.R
    Updated and optimised script:
    
    • Dotted horizontal lines are set up automatically
    • Added option to change currency for title

--- Plots/Scripts for plots/Line Plot for Portfolio Dividends.R
+++ Plots/Scripts for plots/Line Plot for Portfolio Dividends.R
@@ -1,17 +1,24 @@
-p.plt.dividends <- function(x, N){ # Plot showing Dividend Accumulation
+p.plt.dividends <- function(x, c = "$US", sub = NULL){ # Dividend Accumulation
   
   x <- cumsum(x[,ncol(x)]) # Calculate cumulative sum of dividends
   
-  plot(x, main = "Dividend Amount Accumulation (in US$)", col = "red", las = 1,
-       ylab = "", xlab = "Trading Days", sub = "Data Source: Yahoo! Finance",
-       lwd = 3)
+  plot(x, main = sprintf("Dividend Amount Accumulation (in %s)", c), lwd = 3,
+       col="red", las=1, ylab = "", xlab = "Trading Days", sub=sub, type = "l")
   
   par(mar = c(5, 3.5, 5, 3.5)) # Define borders of the plot
   
-  abline(h = 0) # Add black horizontal line showing 0 and grey line for others
-  abline(h = seq(x[nrow(x),], from = N, by = N), col = "grey", lty = 3)
+  m <- round(max(x), 0) / 10 ^ (nchar(round(max(x), 0)) - 1) # Axes Interval
   
-  axis(side = 2, at = seq(x[nrow(x),], from = N, by = N * 2), las = 2) # Axes
-  axis(side = 4, at = seq(x[nrow(x),], from = 0, by = N), las = 2) 
+  if (m > 0 && m < 1){ mn <- 1 * 10 ^ (nchar(m) - 3) }
+  
+  else if (m > 1 && m < 2){ mn <- 2 * 10 ^ (nchar(m) - 3) }
+  
+  else if (m > 2 && m < 5){ mn <- 5 * 10 ^ (nchar(m) - 3) }
+  
+  abline(h = 0) # Add black horizontal line showing 0 and grey line for others
+  abline(h = seq(round(max(x), 0), from = mn/2, by = mn/2), col="grey", lty=3)
+  
+  axis(side = 2, at = seq(round(max(x), 0),  from = mn / 2, by = mn), las = 2) 
+  axis(side = 4, las = 1, at = seq(0, round(max(x), 0), mn / 2)) # Both Axes
 }
-p.plt.dividends(df_portfolio_dividend, N = 10) # Test
+p.plt.dividends(df_portfolio_dividend, sub = "Data Source: Yahoo! Finance")
