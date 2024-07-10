b7e22b96f45b0f31ca74cf9afa128239611219e4
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-07-10 12:43:44 +0500

    Update Dividend Cash Flow.R
    Added Option to include tax and adjust final sum to it

--- Scripts/Portfolio Dividends/Dividend Cash Flow.R
+++ Scripts/Portfolio Dividends/Dividend Cash Flow.R
@@ -1,4 +1,4 @@
-p.df.dividends.cash.flow <- function(x, full = F){ # Dividend Cash Flow
+p.df.dividends.cash.flow <- function(x, full=F, tax = 0){ # Dividend Cash Flow
   
   # Reduce observations without dividend cash flow
   x <- as.data.frame(x[rowSums(x[,3 * seq(ncol(x) %/% 3, from = 1)]) > 0,])
@@ -18,8 +18,10 @@ p.df.dividends.cash.flow <- function(x, full = F){ # Dividend Cash Flow
   
   for (n in (ncol(l) - 2):ncol(l)){ l[,n] <- as.numeric(l[,n]) } # Turn numeric
   
+  if (isTRUE(tax != 0)){ l$`After Tax` <- l[,ncol(l)] * (1 - tax) } # Tax
+  
   l$`Cumulative Total` <- cumsum(l[,ncol(l)]) # Column with Cumulative Total
   
   l # Display
 }
-p.df.dividends.cash.flow(df_portfolio_dividend, full = F) # Test
+p.df.dividends.cash.flow(df_portfolio_dividend, full = F, tax = 0.1) # Test
