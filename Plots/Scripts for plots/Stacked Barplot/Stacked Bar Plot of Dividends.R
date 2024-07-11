c93ec87be52e69c22ea7e976ec422706a1d826f0
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-07-11 15:58:51 +0500

    Update Stacked Bar Plot of Dividends.R
    Updated and optimised script:
    
    • Added option to change currency for dividends on y-axis
    • Renamed variables
    • Reduced excessive lines

--- Plots/Scripts for plots/Stacked Barplot/Stacked Bar Plot of Dividends.R
+++ Plots/Scripts for plots/Stacked Barplot/Stacked Bar Plot of Dividends.R
@@ -1,60 +1,51 @@
 lapply(c("ggplot2", "tidyverse"), require, character.only = T) # Libraries
 
 # Stacked Bar Plot of Portfolio Securities Dividends
-p.bar.plt.stack.dividend <- function(x, portion = F){ 
+p.bar.plt.stack.dividend <- function(x, portion = F, c = "$US"){ 
   
-  f.df <- x[,3 * seq(ncol(x) %/% 3, from = 1)] # Columns with total sum
+  d <- x[,3 * seq(ncol(x) %/% 3, from = 1)] # Columns with total sum
   
   # Take column names with prices to put instead total sum column names
-  colnames(f.df) <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)])[-(ncol(x)%/%3+1)]
+  colnames(d) <- colnames(x[,1+3*seq(ncol(x) %/% 3,from=0)])[-(ncol(x) %/% 3+1)]
   
-  f.df <- f.df[rowSums(f.df) > 0,][,colSums(f.df) > 0] # Only > 0
+  d <- d[rowSums(d) > 0,][,colSums(d) > 0] # Only > 0
   
-  rwnms <- rownames(f.df) # Take dates from index column
+  d <- data.frame(as.Date(rownames(d)), d) # Join date format & main data set
   
-  rwnms <- as.Date(rwnms) # Make it in date format
+  rownames(d) <- seq(nrow(d)) # Create sequence for index column
   
-  f.df <- data.frame(rwnms, f.df) # Join it with main data set
+  D <- NULL # Define variable to contain values
   
-  rownames(f.df) <- seq(nrow(f.df)) # Create sequence for index column
-  
-  p.df <- NULL # Define variable to contain values
-  
-  for (n in 2:ncol(f.df)){ # Convert daily data to monthly
+  for (n in 2:ncol(d)){ v <- tapply(d[,n], format(as.Date(d[,1]),"%Y-%m"), sum)
     
-    v <- tapply(f.df[,n], format(as.Date(f.df[,1]), "%Y-%m"), sum)
-    
-    rwmns_ds <- rownames(v) # Take dates from index column
-    
-    v <- data.frame(rwmns_ds, v) # Join with new data set
+    v <- data.frame(rownames(v), v) # Join with new data set
     
     rownames(v) <- seq(nrow(v)) # Generate sequence for index column
     
-    colnames(v)[colnames(v) == 'rwmns_ds'] <- 'Date' # Name column as Date
+    colnames(v)[1] <- 'Date' # Name column as Date
     
     # If defined empty variable is still empty # Put new dataset there
-    if (is.null(p.df)){ p.df<-v } else { p.df <- merge(x=p.df,y=v,by="Date")} }
-    
-  p.df <- as.data.frame(p.df) # Convert to data frame format
+    if (is.null(D)){ D <- v } else { D <- merge(x = D, y = v, by = "Date") } }
   
-  colnames(p.df) <- colnames(f.df) # Give column names
+  D <- as.data.frame(D) # Convert to data frame format
   
-  colnames(p.df)[colnames(p.df) == colnames(p.df[1])] <- 'Date' # Rename again
+  colnames(D) <- colnames(d) # Give column names
   
-  # Colour set for plot
+  colnames(D)[colnames(D) == colnames(D[1])] <- 'Date' # Rename again
+  
   C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
         "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
         "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
         "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
         "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
-        "#895c8b","#bd5975")
+        "#895c8b","#bd5975") # Colour set 
   
   # Convert for better read by ggplot
-  p.df<-p.df %>% pivot_longer(cols=-Date,names_to="Stock",values_to="Quantity")
+  D <- D %>% pivot_longer(cols=-Date, names_to="Stock", values_to="Quantity")
   
   if (isTRUE(portion)){ # Plot showing stakes of dividends for each month
     
-    ggplot(p.df, aes(x = Date, y = Quantity, fill = Stock)) + theme_minimal() +
+    ggplot(D, aes(x = Date, y = Quantity, fill = Stock)) + theme_minimal() +
       geom_bar(position = "fill", stat = "identity") + 
       labs(title = "Stacked Bar Plot of Portfolio Securities Dividends",
            x = "Months", y = "Stakes (%)", fill = "Securities") +
@@ -62,10 +53,10 @@ p.bar.plt.stack.dividend <- function(x, portion = F){
     
   } else { # Generate plot showing amount of dividends for each month
     
-    ggplot(p.df, aes(x = Date, y = Quantity, fill = Stock)) + theme_minimal() +
+    ggplot(D, aes(x = Date, y = Quantity, fill = Stock)) + theme_minimal() +
       geom_bar(position = "stack", stat = "identity") + 
       labs(title = "Stacked Bar Plot of Portfolio Securities Dividends",
-           x = "Months", y = "Amount in $US", fill = "Securities") +
+           x = "Months", y = sprintf("Amount in %s", c), fill = "Securities") +
       scale_fill_manual(values = C) }
 }
-p.bar.plt.stack.dividend(df_portfolio_dividend, portion = T) # Test
+p.bar.plt.stack.dividend(df_portfolio_dividend, portion = F) # Test
