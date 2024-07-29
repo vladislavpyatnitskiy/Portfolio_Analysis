e5263b0c81bff0ae8c2cefe07b70d370983829ce
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-07-29 12:45:13 +0500

    Update Portfolio Performance by Monte Carlo.R
    Updated and optimised script:
    
    • Added necessary libraries to run the code
    • Added scripts to calculate portfolio return so there is no need to use data frame of returns instead of portfolio data frame 
    • Renamed variable for days from "ndays" to "d"

--- Scripts/Portfolio Performance by Monte Carlo.R
+++ Scripts/Portfolio Performance by Monte Carlo.R
@@ -1,28 +1,48 @@
-p.monte.carlo <- function(c, ndays, n){ # Monte Function
+lapply(c("ggplot2","data.table","timeSeries"),require,character.only=T) # Libs
+
+p.monte.carlo <- function(x, d, n){ # Monte Function
   
-  r <- as.numeric(c + 1) # Calculate returns
+  x <- x[,3 * seq(ncol(x) %/% 3, from = 1)] # Take columns with Total Sum
+  
+  x1 <- t(x) # Transpose x1 and x
+  
+  colnames(x1) <- rownames(x) # Make dates as column names x1 and x
+  
+  r <- as.data.frame(0) # Define data frame with value zero
+  
+  # Loop for portfolio log returns calculation
+  for (m in 2:ncol(x1)){ D <- x1[,(m - 1):m] # x1 # Select two periods
+  
+    s <- D[apply(D, 1, function (row) all(row !=0 )),] # Remove zeros & NA
+  
+  # Add newly generated variable to data frame
+  r <- rbind(r, log(as.numeric(colSums(s)[2]) / as.numeric(colSums(s)[1]))) }
+  
+  colnames(r) <- "Returns" # Give name to column
+  rownames(r) <- rownames(x) # Return dates to index
+  
+  r <- as.timeSeries(r) # Make it time series
+  
+  r <- as.numeric(r + 1) # Calculate returns
   r[1] <- 1 # Assign first observation as 1
   set.seed(0) # Calculate various scenarios of Stock Performance
   
   # Mimic Historical Performance using log returns
-  p <- data.table(apply(replicate(n,expr=round(sample(r,ndays,replace=T),
-                                               2)),2,cumprod))
+  p <- data.table(apply(replicate(n, expr = round(sample(r, d, replace=T), 2)),
+                        2, cumprod))
   p$days <- 1:nrow(p)
   p <- melt(p, id.vars = "days")
   
-  # Make Line Charts with all scenarious
-  monte_graph <- ggplot(p, aes(x=days,y=(value - 1) * 100, col=variable)) +
+  # Make Line Charts with all scenarios
+  monte_graph <- ggplot(p, aes(x=days, y=(value - 1) * 100, col=variable)) +
     geom_line() +
     theme_bw() +
     theme(legend.position = "none") +
     ggtitle("Portfolio Performance by Monte Carlo") +
     xlab("Days Invested") + 
-    ylab("Portfolio Return (%)")
+    ylab("Return (%)")
   
-  monte_summary <- summary((p$value[p$days == ndays] - 1) * 100) # Stats
-  
-  monte_mean <- mean((p$value[p$days] - 1) * 100 < 0) # Expected Return
-  
-  list(monte_graph, monte_summary, monte_mean) # plot & stats
+  list(monte_graph, summary((p$value[p$days == d] - 1) * 100),
+       mean((p$value[p$days] - 1) * 100 < 0)) # plot & stats & mean
 }
-p.monte.carlo(returns_df, 1000, 100) # Test
+p.monte.carlo(df_portfolio, 1000, 100) # Test
