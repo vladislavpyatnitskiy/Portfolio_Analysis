e8a67deb27fe68bd4c4337a978e593c777366230
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-07-30 15:41:25 +0500

    Update Portfolio Info FINVIZ.R
    Optimised script:
    
    • Reduced excessive lines
    • Renamed variables

--- Scripts/Portfolio Securities Data/Portfolio Info FINVIZ.R
+++ Scripts/Portfolio Securities Data/Portfolio Info FINVIZ.R
@@ -4,34 +4,30 @@ p.finviz.ratios <- function(x){ # Get info about tickers from FINVIZ
   
   x <- colnames(x[,1+3*seq(ncol(x) %/% 3,from=0)])[-(ncol(x)%/%3+1)] # Tickers
   
-  df_ <- NULL
+  D <- NULL
   
   for (n in 1:length(x)){ j <- x[n] # Assign security and read HTML of page
-  
-    s <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", j))
     
-    s.yahoo <- s %>% html_nodes('table') %>% .[[10]] -> tab # Assign Table 
+    y <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", j)) %>%
+      html_nodes('table') %>% .[[10]] %>% html_nodes('tr') %>%
+      html_nodes('td') %>% html_text() # Get info from FINVIZ website
     
-    y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    d <- NULL # Create lists to contain values
     
-    df <- NULL # Create lists to contain values
+    for (n in 0:(length(y)/2)){ d <- rbind(d, cbind(y[(1+n*2)], y[(2+n*2)])) } 
     
-    for (n in 0:(length(y) / 2)){ # Ratio values
+    d <- d[-nrow(d),] # Reduce last column
       
-      df <- rbind(df, cbind(y[(1 + n * 2)], y[(2 + n * 2)])) } 
-    
-    df <- df[-nrow(df),] # Reduce last column
-    
-    df[27,1] <- "EPS next Y (%)" # Change name
-    
-    rownames(df) <- df[,1] # Assign row names
-    
-    df <- subset(df, select = -c(1)) # Reduce excess column
-    
-    colnames(df) <- j # Assign column name
-    
-    if (is.null(df_)){ df_ <- df } else { df_ <- cbind(df_, df) } }
-  
-  df_ # Display
+    d[27,1] <- "EPS next Y (%)" # Change name
+      
+    rownames(d) <- d[,1] # Assign row names
+      
+    d <- subset(d, select = -c(1)) # Reduce excess column
+      
+    colnames(d) <- j # Assign column name
+      
+    if (is.null(D)){ D <- d } else { D <- cbind(D, d) } } # Join
+      
+  D # Display
 }
 p.finviz.ratios(df_portfolio) # Test
