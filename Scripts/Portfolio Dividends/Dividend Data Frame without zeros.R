4b117353b0c3bf37c7ea62cc828c54f5b8cc00a7
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-07-30 11:47:26 +0500

    Update Dividend Data Frame without zeros.R
    Optimised script by reducing excessive line

--- Scripts/Portfolio Dividends/Dividend Data Frame without zeros.R
+++ Scripts/Portfolio Dividends/Dividend Data Frame without zeros.R
@@ -1,7 +1,5 @@
 p.df.dividend.no.zeros <- function(x){ # Data Frame without zeros in total sum
   
-  x <- x[rowSums(x[,3 * seq(ncol(x) %/% 3, from = 1)]) > 0,] # Only > 0
-  
-  x # Display
+  x[rowSums(x[,3 * seq(ncol(x) %/% 3, from = 1)]) > 0,] # Display only > 0
 }
 p.df.dividend.no.zeros(df_portfolio_dividend) # Test
