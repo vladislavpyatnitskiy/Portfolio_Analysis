fd4cf79f186c734e12422a5cf555994b7d0a558d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-04 15:33:43 +0500

    Update Institutional Holders of Portfolio Securities.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted FOR loop with Sequence

--- Scripts/Institutional Holders of Portfolio Securities.R
+++ Scripts/Institutional Holders of Portfolio Securities.R
@@ -2,37 +2,29 @@ library("rvest") # Library
 
 p.holders <- function(x){ # info about holders of large positions
   
-  x <- colnames(x[,1+3*seq(ncol(x) %/% 3,from=0)])[-(ncol(x)%/%3+1)] # Tickers
+  x <- colnames(x[,1 + 3 * seq(ncol(x) %/% 3,from = 0)])[-(ncol(x) %/% 3 + 1)] 
   
-  d <- NULL # Where to put data
+  d <- NULL
   
-  for (n in 1:length(x)){ c <- x[n] # Download data for each security
+  for (m in 1:length(x)){ c <- x[m]
+    
+    y <- read_html(sprintf("https://uk.finance.yahoo.com/quote/%s/holders",
+                           c)) %>% html_nodes('table') %>% .[[2]] %>%
+      html_nodes('tr') %>% html_nodes('td') %>% html_text() # Get values
+    
+    v <- cbind.data.frame(y[seq(from = 1, to = length(y), by = 5)],
+                          y[seq(from = 4, to = length(y), by = 5)])
   
-    s <- sprintf("https://finance.yahoo.com/quote/%s/holders?p=%s", c, c)
-    
-    s <- read_html(s) # Read html info
-    
-    s.yahoo <- s %>% html_nodes('table') %>% .[[2]] -> tab # Assign Table 
-    
-    y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-    
-    v <- NULL # Create lists to contain variables & Subset excess row
-    
-    for (n in 0:(length(y) / 5)){ # Scrape data for holders and portions
-      
-      v <- rbind(v, cbind(y[(1+n*5)], read.fwf(textConnection(y[(4+n*5)]),
-                                               widths=c(nchar(y[(4+n*5)])-1,1),
-                                               colClasses = "character"))) } 
-    v <- v[-nrow(v),][,-ncol(v)] 
+    for (n in 1:nrow(v)){ v[n,2] <- strsplit(v[n,2], split = "%") }
     
     v[,2] <- as.numeric(v[,2]) # Change format to numeric
-    
+      
     v[nrow(v) + 1,] = c("Others", 100 - sum(v[,2])) # Numbers for others
-    
+      
     colnames(v) <- c("Top Institutional Holders", c) # Column names
     rownames(v) <- seq(nrow(v)) # Row names
     
-    if (is.null(d)){ d <- v } else { # Join securities' data
+    if (is.null(d)){ d <- v } else { # Full Join
       
       d <- merge(x = d, y = v, by = "Top Institutional Holders", all = T) } }
   
