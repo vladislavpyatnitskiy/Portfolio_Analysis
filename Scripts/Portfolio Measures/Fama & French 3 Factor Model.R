8ede2bfd93161b756ea5ec5965ee6ba5c9fc67f0
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-12 13:22:50 +0500

    Update Fama & French 3 Factor Model.R
    Updated script:
    
    • Substituted data frame of portfolio returns with portfolio data frame

--- Scripts/Portfolio Measures/Fama & French 3 Factor Model.R
+++ Scripts/Portfolio Measures/Fama & French 3 Factor Model.R
@@ -1,6 +1,27 @@
-library(tidyverse) # Library
+lapply(c("timeSeries", "tidyverse"), require, character.only = T) # Libs 
 
 ff3 <- function(x){ # Fama & French 3 Factor Model
+  
+  x <- x[,3 * seq(ncol(x) %/% 3, from = 1)] # Take columns with Total Sum
+  
+  x1 <- t(x) # Transpose x1 and x
+  
+  colnames(x1) <- rownames(x) # Make dates as column names x1 and x
+  
+  r <- as.data.frame(0) # Define dataframe with value zero
+  
+  # Loop for portfolio log returns calculation
+  for (n in 2:ncol(x1)){ df2p <- x1[,(n-1):n] # x1 # Select two periods
+  
+    s <- df2p[apply(df2p, 1, function(row) all(row !=0 )),] # Remove zeros & NA
+  
+  # Add newly generated variable to data frame
+  r <- rbind(r, log(as.numeric(colSums(s)[2]) / as.numeric(colSums(s)[1]))) }
+  
+  colnames(r) <- "Returns" # Give name to column
+  rownames(r) <- rownames(x) # Return dates to index
+  
+  r <- as.timeSeries(r) # Make it time series
   
   temp <- tempfile() # Assign temporal file
   
@@ -20,7 +41,7 @@ ff3 <- function(x){ # Fama & French 3 Factor Model
   D <- NULL # Split second column into 4 separate ones
   
   for (n in 1:nrow(f)){ # 
-  
+    
     D <- rbind.data.frame(D, unlist(strsplit(as.character(f[n,2]), "\\,"))) }
   
   D <- data.frame(f[,1], D) # Join Dates with data
@@ -29,7 +50,7 @@ ff3 <- function(x){ # Fama & French 3 Factor Model
   
   colnames(D) <- c("Dates","MRKT", "SMB", "HML", "RF") # Column names
   
-  R <- data.frame(rownames(x), x) # Join Dates with Portfolio Returns data
+  R <- data.frame(rownames(r), r) # Join Dates with Portfolio Returns data
   
   colnames(R) <- c("Dates", "Returns") # Assign column names
   
@@ -43,4 +64,4 @@ ff3 <- function(x){ # Fama & French 3 Factor Model
   
   summary(lm(M$RPRF ~ M$MRKT + M$SMB + M$HML)) # Run regression
 }
-ff3(returns_df) # Test
+ff3(df_portfolio) # Test
