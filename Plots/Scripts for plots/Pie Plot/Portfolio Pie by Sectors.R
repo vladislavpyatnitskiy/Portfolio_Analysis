abc0cda636a9cd6b01bcf89e2de9ccdf82107ca1
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-09-03 11:24:51 +0500

    Update Portfolio Pie by Sectors.R
    Substituted Yahoo! Finance with stockanalytics website

--- Plots/Scripts for plots/Pie Plot/Portfolio Pie by Sectors.R
+++ Plots/Scripts for plots/Pie Plot/Portfolio Pie by Sectors.R
@@ -2,41 +2,40 @@ library("rvest")
 
 p.pie.plt.sector <- function(x, main = NULL, sub = NULL){ 
   
-  tickers <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)])[-(ncol(x)%/%3+1)]
+  y <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)])[-(ncol(x)%/%3+1)]
   
   pct <- as.data.frame(x[,3 * seq(ncol(x) %/% 3, from = 1)]) / x[,ncol(x)]
   
-  colnames(pct) <- tickers # Assign tickers
+  colnames(pct) <- y # Assign tickers
   
   pct <- t(round(pct[nrow(pct),] * 100)) # Transform last period into percents
   
-  # Add colour range
   C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
         "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
         "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
         "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
         "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
-        "#895c8b","#bd5975")
+        "#895c8b","#bd5975") # Add colour range
   
   colnames(pct) <- "Portion" # Assign column name
   
   l <- NULL # Create list
   
-  for (n in 1:length(tickers)){ # Read HTML & extract necessary info
+  for (n in 1:length(y)){ k <- y[n] # Read HTML & extract necessary info
+    
+    p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/", tolower(k)))
+    
+    p <- p %>% html_nodes('body') %>% html_nodes('main') %>%
+      html_nodes('div') %>% html_nodes('a') 
+    
+    q <- p %>% html_attr('class') == "dothref text-default" # Clean data
+    
+    S <- p[grep("TRUE", q)] %>% html_text()
+    
+    l <- rbind.data.frame(l, S[2]) } # Join 
   
-    p <- read_html(sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s",
-                           tickers[n], tickers[n])) 
-    
-    price.yahoo1 <- p %>% html_nodes('div') %>% .[[1]] -> tab
-    
-    y <- tab %>% html_nodes('p') %>% html_nodes('span') %>% html_text()
-    
-    l <- rbind(l, y[2]) } # Add to list
-    
   colnames(l) <- "Sector" # Assign column name
-  rownames(l) <- tickers # Assign tickers
-  
-  l # Display
+  rownames(l) <- y # Assign tickers
   
   pie.df <- data.frame(l, pct) # Form data frame
   pie.df <- aggregate(Portion ~ Sector, data=pie.df, sum) # Conditional sum
