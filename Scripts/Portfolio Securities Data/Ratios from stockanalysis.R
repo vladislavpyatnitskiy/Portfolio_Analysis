8b63d12d19f3ca3cb237bcfd8ef130d57589adfe
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-12-10 11:49:30 +0500

    Update Ratios from stockanalysis.R
    Reduced line for tickers assigning

--- Scripts/Portfolio Securities Data/Ratios from stockanalysis.R
+++ Scripts/Portfolio Securities Data/Ratios from stockanalysis.R
@@ -1,7 +1,7 @@
 library(rvest) # Library
 
 p.ratios.sa <- function(x){ # Ratios data from stockanalysis website
- 
+  
   x <- colnames(x[,1 + 3 * seq(ncol(x) %/% 3,from = 0)])[-(ncol(x) %/% 3 + 1)]
   
   L <- NULL
@@ -12,10 +12,9 @@ p.ratios.sa <- function(x){ # Ratios data from stockanalysis website
                            tolower(x[n]))) %>% html_nodes('table') %>%
       html_nodes('tr') %>% html_nodes('td') %>% html_text()
     
-    R <- p[seq(from = 1, to = length(p), by = 2)] # row names
     C <- as.data.frame(p[seq(from = 2, to = length(p), by = 2)]) # stock data
     
-    rownames(C) <- R # tickers as row names  
+    rownames(C) <- p[seq(from=1, to=length(p), by=2)] # tickers as row names  
     
     if (is.null(L)){ L <- C } else { L <- cbind.data.frame(L, C) } }
   
