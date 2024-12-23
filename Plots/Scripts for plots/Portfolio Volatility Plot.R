4fc32a04ebd67c54b4cb5a2ceccad52c92134c8b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-12-23 11:40:32 +0500

    Update Portfolio Volatility Plot.R
    Rewrote script:
    
    • Input is substituted from returns data frame to portfolio data frame
    • Grid part is rewritten to more suitable one
    • Added "timeSeries" library

--- Plots/Scripts for plots/Portfolio Volatility Plot.R
+++ Plots/Scripts for plots/Portfolio Volatility Plot.R
@@ -1,6 +1,26 @@
+library(timeSeries) # Library
+
 p.volatility.plt <- function(x, abs = F){ # Plot Security volatility
   
-  x <- x * 100 # Multiply by 100
+  x <- x[,3*seq(ncol(x)%/%3,from=1)] # Subtract total values for each stock
+  
+  x1 <- t(x) # Transpose data frame
+  
+  colnames(x1) <- rownames(x) # Make dates as column names x1 and x
+  
+  r <- as.data.frame(0) # Define data frame with value zero
+  
+  # Loop for portfolio log returns calculation
+  for (n in 2:ncol(x1)){ df2p <- x1[,(n-1):n] # x1 # Select two periods
+  
+    s <- df2p[apply(df2p, 1, function(row) all(row !=0 )),] # Remove zeros & NA
+    
+    r <- rbind(r, log(colSums(s)[2]/colSums(s)[1])) } # Add value to data frame
+    
+  colnames(r) <- "Returns" # Give name to column
+  rownames(r) <- rownames(x) # Return dates to index
+  
+  x <- as.timeSeries(r) * 100 # Make it time series & multiply by 100
   
   if (isFALSE(abs)){ # Choose between real and absolute values of returns
     
@@ -10,44 +30,20 @@ p.volatility.plt <- function(x, abs = F){ # Plot Security volatility
            main = sprintf("Volatility of Portfolio %s", colnames(x[,n])),
            ylim = c(min(x[,n]), max(x[,n])), las = 2)
       
-      abline(h = 0)
-      
-      if (max(x[,n]) < 10){
-        
-        axis(side = 2, at = seq(-100, 100, 1), las = 2) # Set up axis
-        abline(h = seq(-100, -1, 1), col = "grey", lty = 3)
-        abline(h = seq(1, 100, 1), col = "grey", lty = 3) }
-      
-      else if (max(x[,n]) < 15){
-        
-        axis(side = 2, at = seq(-100, 100, 2), las = 2) # Set up axis
-        abline(h = seq(-100, -2, 2), col = "grey", lty = 3)
-        abline(h = seq(2, 100, 2), col = "grey", lty = 3) } else {
-          
-          axis(side = 2, at=seq(100, from = 5, by = 5), las = 2) # Set up axis
-          abline(h = seq(-100, -5, 5), col = "grey", lty = 3)
-          abline(h = seq(5, 100, 5), col = "grey", lty = 3) } } } else {
-        
-      x <- abs(x)
-        
+      } } else { x <- abs(x)
+            
       for (n in 1:ncol(x)){ # Plot returns fluctuations
-          
+              
         plot(x[,n], col = "red", ylab = "Fluctuations (%)", las = 2,
              xlab = "Trading Days", ylim = c(min(x[,n]), max(x[,n])),
-             main = sprintf("Portfolio %s Fluctuations", colnames(x[,n])))
-        
-        abline(h = 0)
-        
-        if (max(x[,n]) < 10){ axis(side = 2, at = seq(-100, 100, 1), las = 2) 
-        
-          abline(h = seq(1, 100, 1), col = "grey", lty = 3) }
-          
-        else if (max(x[,n]) < 15){
-            
-          axis(side = 2, at = seq(-100, 100, 2), las = 2) # Set up axis
-          abline(h = seq(2, 100, 2), col = "grey", lty = 3) } else {
-              
-          axis(side = 2, at=seq(100, from = 5, by = 5), las = 2) # Set up axis
-          abline(h = seq(5, 100, 5), col = "grey", lty = 3) } } }
+             main = sprintf("Portfolio %s Fluctuations", colnames(x[,n]))) } }
+  
+  abline(h = 0)
+  
+  axis(side = 4, las = 2)
+  
+  grid(nx = 1, ny = NULL, col = "grey", lty = "dotted", lwd = 1)
+  
+  par(mar = c(5, 4, 4, 4)) # Define borders of the plot to fit right y-axis
 }
-p.volatility.plt(returns_df, abs = F)
+p.volatility.plt(df_portfolio, abs = F)
