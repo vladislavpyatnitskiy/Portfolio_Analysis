2a3f7ffc8bb2d1a3e9f95e5ac8ef7148a323af5a
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-02 13:56:41 +0500

    Update Stacked Barplot.R
    Optimised script:
    
    • Reduced excessive lines
    • Renamed variables
    • Edited comments

--- Plots/Scripts for plots/Stacked Barplot/Stacked Barplot.R
+++ Plots/Scripts for plots/Stacked Barplot/Stacked Barplot.R
@@ -1,66 +1,55 @@
-lapply(c("ggplot2", "tidyverse"), require, character.only = T) # Libraries
+lapply(c("ggplot2", "tidyverse"), require, character.only = T) # Libs
 
 p.bar.plt.stack <- function(x, portion = T){ # Stacked Bar Plot
   
-  f.df <- x[,3 * seq(ncol(x) %/% 3, from = 1)] # Subset columns with total sum
+  d <- x[,3 * seq(ncol(x) %/% 3, from = 1)] # Subset columns with total sum
   
   # Take column names with prices to put instead total sum column names
-  colnames(f.df) <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)])[-(ncol(x)%/%3+1)]
+  colnames(d) <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)])[-(ncol(x)%/%3+1)]
   
-  rwnms <- rownames(f.df) # Take dates from index column
+  d <- data.frame(as.Date(rownames(d)), d) # Join time series with data frame
   
-  rwnms <- as.Date(rwnms) # Make it in date format
+  rownames(d) <- seq(nrow(d)) # Create sequence for index column
   
-  f.df <- data.frame(rwnms, f.df) # Join it with main data set
+  D <- NULL # Define variable to contain values
   
-  rownames(f.df) <- seq(nrow(f.df)) # Create sequence for index column
-  
-  p.df <- NULL # Define variable to contain values
-  
-  for (n in 2:ncol(f.df)){ # Convert daily data to monthly
-  
-    v <- tapply(f.df[,n], format(as.Date(f.df[,1]), "%Y-%m"), median)
+  for (n in 2:ncol(d)){ # Convert daily data to monthly
     
-    rwmns_ds <- rownames(v) # Take dates from index column
+    v <- tapply(d[,n], format(as.Date(d[,1]), "%Y-%m"), median)
     
-    v <- data.frame(rwmns_ds, v) # Join with new data set
+    R <- rownames(v) # Take dates from index column
+    
+    v <- data.frame(R, v) # Join with new data set
     
     rownames(v) <- seq(nrow(v)) # Generate sequence for index column
     
-    colnames(v)[colnames(v) == 'rwmns_ds'] <- 'Date' # Name column as Date
+    colnames(v)[colnames(v) == 'R'] <- 'Date' # Name column as Date
     
     # If defined empty variable is still empty # Put new dataset there
-    if (is.null(p.df)){ p.df<-v } else { p.df <- merge(x=p.df,y=v,by="Date")} }
-    
-  p.df <- as.data.frame(p.df) # Convert to data frame format
+    if (is.null(D)){ D <- v } else { D <- merge(x = D, y = v, by = "Date") } }
   
-  colnames(p.df) <- colnames(f.df) # Give column names
+  D <- as.data.frame(D) # Convert to data frame format
   
-  colnames(p.df)[colnames(p.df) == colnames(p.df[1])] <- 'Date' # Rename again
+  colnames(D) <- colnames(d) # Give column names
   
-  # Colour set for plot
+  colnames(D)[colnames(D) == colnames(D[1])] <- 'Date' # Rename again
+  
   C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
         "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
         "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
         "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
         "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
-        "#895c8b","#bd5975")
+        "#895c8b","#bd5975") # Colour set for plot
   
   # Convert for better read by ggplot
-  p.df<-p.df %>% pivot_longer(cols=-Date,names_to="Stock",values_to="Quantity")
+  D <- D %>% pivot_longer(cols=-Date, names_to="Stock", values_to="Quantity")
   
-  if (isTRUE(portion)){ # Plot showing stakes of securities for each month
-    
-    ggplot(p.df, aes(x = Date, y = Quantity, fill = Stock)) + theme_minimal() +
-      geom_bar(position = "fill", stat = "identity") + 
-      labs(title = "Stacked Bar Plot of Portfolio Securities", x = "Months",
-           y="Stakes (%)", fill="Securities") + scale_fill_manual(values=C)   
-    
-  } else { # Generate plot showing amount of securities for each month
-    
-    ggplot(p.df, aes(x = Date, y = Quantity, fill = Stock)) + theme_minimal() +
-      geom_bar(position = "stack", stat = "identity") + 
-      labs(title = "Stacked Bar Plot of Portfolio Securities", x = "Months",
-           y="Amount in $US", fill="Securities") + scale_fill_manual(values=C)}
+  if (isTRUE(portion)){ l <- c("fill", "Stakes") } # Portions
+  
+  else { l <- c("stack", "Amount in $US") } # By Value
+  
+  ggplot(D, aes(x = Date, y = Quantity, fill = Stock)) + theme_minimal() +
+    geom_bar(position = l[1], stat="identity") + scale_fill_manual(values=C) +
+    labs(title="Portfolio Securities", x="Months", y=l[2], fill="Securities") 
 }  
-p.bar.plt.stack(df_portfolio, portion = F) # Test
+p.bar.plt.stack(df_portfolio, portion = T) # Test
