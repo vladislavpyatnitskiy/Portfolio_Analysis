f9f5641e072bbe4c02f14bfd55b99ee226ca38cd
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-05 23:06:51 +0500

    Update Total Sum Data Frame.R
    Reduced excessive line

--- Scripts/Portfolio Securities Data/Total Sum Data Frame.R
+++ Scripts/Portfolio Securities Data/Total Sum Data Frame.R
@@ -1,13 +1,10 @@
 p.position.info <- function(x){ # Info of Total Sum for each Stock
   
-  # Tickers
-  d <- colnames(x[,1 + 3 * seq(ncol(x) %/% 3, from = 0)][,1:(ncol(x) %/% 3)])
-  
   # Total sum for each security
   D <- t(as.data.frame(x[,3*seq(ncol(x)%/%3, from=1)][nrow(x),]))
   
-  rownames(D) <- d
-  colnames(D) <- "Total"
+  rownames(D) <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)][,1:(ncol(x)%/%3)])
+  colnames(D) <- "Total" # Tickers and total amount for each stock
   
   D # Display
 }
