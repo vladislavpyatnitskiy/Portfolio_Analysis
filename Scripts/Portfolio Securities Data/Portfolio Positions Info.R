8820b22c8ba6d907a498cfa581b4b400b0e750a4
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-06 12:56:50 +0500

    Update Portfolio Positions Info.R
    Optimised script:
    
    • Reduced excessive lines
    • Edited comments

--- Scripts/Portfolio Securities Data/Portfolio Positions Info.R
+++ Scripts/Portfolio Securities Data/Portfolio Positions Info.R
@@ -1,26 +1,14 @@
 p.position.info.full <- function(x){ # Info of Total Sum for each Stock
   
-  # Tickers
-  d <- colnames(x[,1 + 3 * seq(ncol(x) %/% 3, from = 0)][,1:(ncol(x) %/% 3)])
+  s = x[,1+3*seq(ncol(x)%/%3,from=0)][nrow(x),] # Price, Number & Sum per Stock
+  q = t(as.data.frame(x[,2 + 3 * seq(ncol(x) %/% 3 - 1, from = 0)][nrow(x),]))
+  P = t(as.data.frame(x[,3 + 3 * seq(ncol(x) %/% 3 - 1, from = 0)][nrow(x),]))
   
-  s <- x[,1 + 3*seq(ncol(x)%/%3, from=0)][nrow(x),] # Price per Stock
+  D <- cbind.data.frame(as.data.frame(s[-length(s)]), q, P) # Join
   
-  # Number of each Stock
-  q <- t(as.data.frame(x[,2 + 3 * seq(ncol(x) %/% 3 - 1, from = 0)][nrow(x),]))
-  
-  s <- as.data.frame(s[-length(s)]) # Reduce total sum of all securities
-  
-  # Total sum for each security
-  P <- t(as.data.frame(x[,3 + 3 * seq(ncol(x) %/% 3 - 1, from = 0)][nrow(x),]))
-  
-  rownames(s) <- d # Give common row names as tickers to join data
-  rownames(q) <- d
-  rownames(P) <- d
-  
-  D <- cbind.data.frame(s, q, P) # Join
-  
-  colnames(D) <- c("Price", "Quantity", "Total Sum") # Column names
+  colnames(D) <- c("Price", "Quantity", "Total Sum") # Column & Row names
+  rownames(D) <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)][,1:(ncol(x)%/%3)]) 
   
   D # Display
 }
-p.position.info.full(df_portfolio) # test
+p.position.info.full(df_portfolio) # Test
