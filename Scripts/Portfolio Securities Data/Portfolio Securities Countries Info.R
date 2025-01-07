6dedbf081b3c625cbb61f5452d23972b70b17904
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-07 12:06:43 +0500

    Update Portfolio Securities Countries Info.R
    Rewrote script:
    
    • Substituted source from yahoo! finance to stockanalytics

--- Scripts/Portfolio Securities Data/Portfolio Securities Countries Info.R
+++ Scripts/Portfolio Securities Data/Portfolio Securities Countries Info.R
@@ -4,17 +4,16 @@ p.country <- function(x){ l <- NULL # Create list
 
   x <- colnames(x[,1+3*seq(ncol(x) %/% 3,from=0)])[-(ncol(x)%/%3+1)] # Tickers
   
-  for (n in 1:length(x)){ s <- x[n] # Read HTML & extract necessary info
+  x[grep("VSTO", x)] <- "GEAR" # Substitute expired ticker with new one
   
-    p <- read_html(sprintf("https://uk.finance.yahoo.com/quote/%s/profile", s))
+  for (n in 1:length(x)){ s <- x[n] # Read HTML & extract necessary info
     
-    tab <- p %>% html_nodes('div') %>% .[[1]]
-    
-    y <- tab %>% html_nodes('p') # Find country character in elements
-    
-    l <- rbind(l, strsplit(toString(y),
-                           "<br>")[[1]][length(strsplit(toString(y),
-                                                        "<br>")[[1]])-4]) }
+    C<-read_html(sprintf("https://stockanalysis.com/stocks/%s/company/",s)) %>%
+      html_nodes('table') %>% .[[1]] %>% html_nodes('tr') %>%
+      html_nodes('td') %>% html_text() 
+  
+    l <- rbind.data.frame(l, C[grep("Country", C) + 1]) } # Data Frame 
+  
   colnames(l) <- "Country"
   rownames(l) <- x
   
