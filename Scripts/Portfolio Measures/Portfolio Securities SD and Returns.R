fcd975f585e78fb4a0f749b2dfc0d3bf13e79431
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-15 12:09:33 +0500

    Update Portfolio Securities SD and Returns.R
    Reduced excessive line

--- Scripts/Portfolio Measures/Portfolio Securities SD and Returns.R
+++ Scripts/Portfolio Measures/Portfolio Securities SD and Returns.R
@@ -1,19 +1,17 @@
 p.sd.return <- function(x){ # Data Frame with Standard Deviations and Returns
   
-  tickers <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)][,1:(ncol(x)%/%3)])
-  
-  x <- x[,1 + 3*seq(ncol(x) %/% 3, from = 0)][,1:(ncol(x)%/%3)] # Securities
+  A <- x[,1 + 3*seq(ncol(x) %/% 3, from = 0)][,1:(ncol(x)%/%3)] # Securities
   
   d <- NULL # Empty variable to contain values
   
-  for (n in 1:ncol(x)){ s <- x[,n] # For each security in data frame
+  for (n in 1:ncol(A)){ s <- A[,n] # For each security in data frame
   
     # Clean data to reduce NA and calculate return for ownership period  
     j <- diff(log(s[apply(s, 1, function(row) all(row !=0 )),]))[-1,]
     
     d <- rbind.data.frame(d, cbind(sd(j)*1000, (exp(sum(j))-1)*100)) } # Join
     
-  rownames(d) <- tickers # Tickers
+  rownames(d) <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)][,1:(ncol(x)%/%3)])
   colnames(d) <- c("Standard Deviation", "Return") # Column names
   
   d # Display
