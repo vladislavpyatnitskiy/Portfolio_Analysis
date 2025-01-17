a1fb5db481c84bd9386a421cd3aedeca2bba2ccc
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-17 12:03:12 +0500

    Update Portfolio Weight Values.R
    Optimised script:
    
    • Rewrote part for calculation for each company

--- Scripts/Portfolio Measures/Portfolio Weight Values.R
+++ Scripts/Portfolio Measures/Portfolio Weight Values.R
@@ -1,12 +1,10 @@
-p.weights <- function(x){ # Get Portfolio Weight Values
-  
-  tickers <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)][,1:(ncol(x)%/%3)])
+p.weights <- function(x){ # Portfolio Weight Values
   
   # Take values of last observations and divide by the total sum
-  w <- round(t(as.data.frame(x[,3*seq(ncol(x)%/%3,from=1)][nrow(x),]/
-                               as.numeric(x[nrow(x),ncol(x)]))) * 100, 2)
+  w <- x[,3*seq(ncol(x)%/%3,from=1)][nrow(x),] / as.numeric(x[nrow(x),ncol(x)])
+  w <- round(t(as.data.frame(w)) * 100, 2)
   
-  rownames(w) <- tickers # Assign tickers to this data frame
+  rownames(w) <- colnames(x[,1+3*seq(ncol(x)%/%3,from=0)][,1:(ncol(x)%/%3)])
   colnames(w) <- "Weights (%)" 
   
   w # Display
