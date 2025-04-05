9c52457563d01ad2930ba3f8d01612e3bce52cb6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-05 11:40:56 +0500

    Update Earning Dates.R
    Updated script:
    
    • Substituted "/" with "-" for dates
    • Added option to transform data frame into time series with companies

--- Scripts/Portfolio Securities Data/Earning Dates.R
+++ Scripts/Portfolio Securities Data/Earning Dates.R
@@ -1,6 +1,6 @@
 library(rvest) # Library
 
-p.earnings <- function(x, sort=T, desc=F){ # Earning Dates for Portfolio Stocks
+p.earnings <- function(x, sort=T, desc=F, ts=F){ # Earning Dates for Portfolio 
   
   x <- colnames(x[,1+3*seq(ncol(x) %/% 3,from=0)])[-(ncol(x)%/%3+1)] # Tickers
   
@@ -13,7 +13,7 @@ p.earnings <- function(x, sort=T, desc=F){ # Earning Dates for Portfolio Stocks
       html_nodes('tr') %>% html_nodes('td') %>% html_text()
     
     l <- c(l, format(as.Date(p[grep("Earnings Date", p) + 1],
-                             format = "%B %d, %Y"), "%Y/%m/%d")) }
+                             format = "%B %d, %Y"), "%Y-%m-%d")) }
   names(l) <- x # Tickers
   
   if (isTRUE(sort)){ # If needed sorted, choose between asc and desc
@@ -24,6 +24,31 @@ p.earnings <- function(x, sort=T, desc=F){ # Earning Dates for Portfolio Stocks
   
   colnames(l) <- "Earnings Date" # Earning Date column name
   
+  if (isTRUE(ts)){ U <- unique(l[,1]) # Find unique values for dates
+    
+    tickers <- rownames(l) # Assign new variable to contain values for tickers
+    
+    d <- l[,1] # Reduce data frame to vector
+    
+    names(d) <- tickers # Assign names to vector
+    
+    m <- NULL # Group Stocks by Earning Dates
+    
+    for (n in 1:length(U)){ # Assign tickers to each date:
+      
+      if (isFALSE(identical(names(which(d == U[n])), character(0)))){
+        
+        m <- rbind.data.frame(m, cbind(U[n],
+                                       toString(names(which(d == U[n]))))) } }
+    
+    rownames(m) <- m[,1] # Assign dates as row names
+    
+    m <- subset(m, select = -c(1)) # Reduce excess column
+    
+    colnames(m) <- "Companies" # Column name
+    
+    l <- m  
+  }
   l # Display
 }
 p.earnings(df_portfolio) # Test
