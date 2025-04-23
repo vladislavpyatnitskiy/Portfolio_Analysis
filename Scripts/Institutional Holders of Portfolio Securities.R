fc7364d0bd63af0a2a69ee15bc59b8e7263ebc6a
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-23 11:31:31 +0500

    Update Institutional Holders of Portfolio Securities.R
    Revived script:
    
    • Added new libraries (httr and xml2)

--- Scripts/Institutional Holders of Portfolio Securities.R
+++ Scripts/Institutional Holders of Portfolio Securities.R
@@ -1,33 +1,43 @@
-library("rvest") # Library
+lapply(c("rvest", "httr", "xml2"), require, character.only = T) # Libs
 
 p.holders <- function(x){ # info about holders of large positions
   
   x <- colnames(x[,1 + 3 * seq(ncol(x) %/% 3,from = 0)])[-(ncol(x) %/% 3 + 1)] 
   
+  x <- x[-grep("VSTO", x)] 
+  x <- x[-grep("ARCH", x)] 
+  
   d <- NULL
   
-  for (m in 1:length(x)){ c <- x[m]
+  for (m in 1:length(x)){ s <- x[m]
     
-    y <- read_html(sprintf("https://uk.finance.yahoo.com/quote/%s/holders",
-                           c)) %>% html_nodes('table') %>% .[[2]] %>%
-      html_nodes('tr') %>% html_nodes('td') %>% html_text() # Get values
+    B <- paste("Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+               "AppleWebKit/537.36", "Chrome/122.0.0.0", "Safari/537.36",
+               sep = " ")
     
-    v <- cbind.data.frame(y[seq(from = 1, to = length(y), by = 5)],
-                          y[seq(from = 4, to = length(y), by = 5)])
-  
+    response <- GET(sprintf("https://uk.finance.yahoo.com/quote/%s/holders/",
+                            s), add_headers(`User-Agent` = B))
+    
+    f <- read_html(content(response, as = "text", encoding = "UTF-8")) %>%
+      html_nodes('table') %>% .[[2]] %>% html_nodes('tr') %>%
+      html_nodes('td') %>% html_text() # Get values
+    
+    v <- cbind.data.frame(f[seq(from = 1, to = length(f), by = 5)],
+                          f[seq(from = 4, to = length(f), by = 5)])
+    
     for (n in 1:nrow(v)){ v[n,2] <- strsplit(v[n,2], split = "%") }
     
     v[,2] <- as.numeric(v[,2]) # Change format to numeric
-      
+    
     v[nrow(v) + 1,] = c("Others", 100 - sum(v[,2])) # Numbers for others
-      
-    colnames(v) <- c("Top Institutional Holders", c) # Column names
+    
+    colnames(v) <- c("Top Institutional Holders", s) 
     rownames(v) <- seq(nrow(v)) # Row names
     
     if (is.null(d)){ d <- v } else { # Full Join
       
       d <- merge(x = d, y = v, by = "Top Institutional Holders", all = T) } }
-  
+    
   d # Display
 }
 View(p.holders(df_portfolio)) # Test
