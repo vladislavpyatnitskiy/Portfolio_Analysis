7bce09e39fd3db49fe5c4403655e57bedc70822c
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-06-24 11:55:32 +0500

    Update Indices Info.R
    Added option to aggregate to see stocks assigned to each index (Default Data Frame shows indices for each stock)

--- Scripts/Portfolio Securities Data/Indices Info.R
+++ Scripts/Portfolio Securities Data/Indices Info.R
@@ -1,6 +1,6 @@
 library("rvest") # Library
 
-p.index <- function(x){ # Indices where portfolio securities are included
+p.index <- function(x, agg = F){ # Indices of portfolio securities
   
   A <- colnames(x[,1 + 3 * seq(ncol(x) %/% 3, from = 0)])[-(ncol(x) %/% 3 + 1)]
   
@@ -22,6 +22,17 @@ p.index <- function(x){ # Indices where portfolio securities are included
   
   l[,1] <- gsub("-", "Not Included in Any Index", l[,1]) # Substitution
   
+  if (agg){ df <- cbind.data.frame(rownames(l), l) # Merge tickers with indices
+    
+    rownames(df) <- seq(nrow(df)) # Assign default row names
+    colnames(df) <- c("Ticker", "Index") # Assign column names
+    
+    # Group by Index and combine tickers
+    aggregated <- aggregate(Ticker ~ Index, data = df,
+      FUN = function(tickers) { paste(sort(tickers), collapse = ", ") })
+    
+    l <- aggregated[order(aggregated$Index), ] } # Sort by Index
+  
   l # Display
 }
-p.index(df_portfolio) # Test
+p.index(df_portfolio, T) # Test
