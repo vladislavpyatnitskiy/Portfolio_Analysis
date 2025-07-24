6dc631e39025451b4523ebbce235a91187af25ce
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-24 10:32:14 +0500

    Update M^2.R
    Rewrote script:
    
    • Replaced input for returns data frame to portfolio one
    • Added libraries

--- Scripts/Portfolio Measures/M^2.R
+++ Scripts/Portfolio Measures/M^2.R
@@ -1,10 +1,32 @@
+lapply(c("quantmod", "timeSeries"), require, character.only = T) # Libs
+
 m.squared <- function(x, spx = c("^GSPC", "^IXIC", "^DJI"), tr = "^TNX",
                       benchnames = NULL){ # M squared for portfolio
+  
+  x <- x[,3 * seq(ncol(x) %/% 3, from = 1)] # Take columns with Total Sum
+  
+  x1 <- t(x) # Transpose x1 and x
+  
+  colnames(x1) <- rownames(x) # Make dates as column names x1 and x
+  
+  r <- as.data.frame(0) # Define dataframe with value zero
+  
+  # Loop for portfolio log returns calculation
+  for (n in 2:ncol(x1)){ df2p <- x1[,(n-1):n] # x1 # Select two periods
+  
+    s <- df2p[apply(df2p, 1, function(row) all(row !=0 )),] # Remove zeros & NA
+  
+    # Add newly generated variable to data frame
+    r <- rbind(r, log(as.numeric(colSums(s)[2]) / as.numeric(colSums(s)[1]))) }
+  
+  colnames(r) <- "Returns" # Give name to column
+  rownames(r) <- rownames(x) # Return dates to index
+  
+  x <- as.timeSeries(r) # Make it time series
   
   y <- c(tr, spx) # Join treasuries and index data
   
   s <- rownames(x)[1] # Assign first date
-  
   e <- rownames(x)[nrow(x)] # Assign last date
   
   p <- NULL # Create an empty variable
@@ -24,18 +46,17 @@ m.squared <- function(x, spx = c("^GSPC", "^IXIC", "^DJI"), tr = "^TNX",
   
   rf <- apply(p[,tr], 2, function(col) mean(col) / 100) # Risk Free
   
-  x <- merge(x,i)[-nrow(x),] # Join portfolio and index data
+  x <- merge(x, i)[-nrow(x),] # Join portfolio and index data
   
   l <- NULL # Set list for values
   
-  for (n in 2:ncol(x)){ # Calculate ratios for each benchmark
+  for (n in 2:ncol(x)) # Calculate ratios for each benchmark
+    
+    l = rbind(l, (exp(sum(x[,1]))-rf)*sd(x[,n])/sd(x[,1])-(exp(sum(x[,n])-rf)))
   
-    l<-cbind(l,(exp(sum(x[,1]))-rf)*sd(x[,n])/sd(x[,1])-(exp(sum(x[,n])-rf))) }
-  
-  rownames(l) <- "M ^ 2" # Give name
-  
-  colnames(l) <- benchnames # Give bench names
+  colnames(l) <- "M ^ 2" # Give name
+  rownames(l) <- benchnames # Give bench names
   
   l # Display value
 }
-m.squared(returns_df, benchnames = c("S&P 500", "NASDAQ", "Dow Jones")) # Test
+m.squared(df_portfolio, benchnames=c("S&P 500", "NASDAQ", "Dow Jones")) # Test
