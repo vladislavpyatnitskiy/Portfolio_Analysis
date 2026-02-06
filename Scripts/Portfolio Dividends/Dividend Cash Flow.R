cbe898f767e2accc62b7c679e8b0b02eb264a626
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-02-06 10:55:24 +0500

    Update Dividend Cash Flow.R
    Improved script readability

--- Scripts/Portfolio Dividends/Dividend Cash Flow.R
+++ Scripts/Portfolio Dividends/Dividend Cash Flow.R
@@ -5,20 +5,28 @@ p.df.dividends.cash.flow <- function(x, full=F, tax = 0){ # Dividend Cash Flow
   
   l <- NULL # Data frame with columns if Date, Ticker, Dividend, Number and Sum
   
-  for (n in 1:nrow(x)){ for (m in 0:((ncol(x) - 4) / 3)){ 
+  for (n in 1:nrow(x)){ 
     
-      if (isTRUE(x[n, (m * 3) + 1] != 0) && isTRUE(x[n, (m * 3) + 2] != 0) &&
-          isTRUE(x[n, (m * 3) + 3] != 0)){
-        
-        l <- rbind.data.frame(l, cbind(rownames(x)[n], colnames(x)[(m * 3) + 1],
-                                       x[n, (m * 3) + 1], x[n, (m * 3) + 2],
-                                       x[n, (m * 3) + 3])) } } } 
+    for (m in 0:((ncol(x) - 4) / 3)){ M = m * 3
+      
+      f = x[n, M + 1] # Dividend amount from one stock
+      s = x[n, M + 2] # Number of shares for one stock
+      th = x[n, M + 3] # Total dividends from one stock
+    
+      if (f != 0 & s != 0  & th != 0){
+      
+        l <- rbind.data.frame(
+          l, cbind(rownames(x)[n], colnames(x)[M + 1], f, s, th)
+          )
+      } 
+    } 
+  } 
   
   colnames(l) <- c("Date", "Ticker", "Dividend", "Number", "Total") # Columns
   
   for (n in (ncol(l) - 2):ncol(l)){ l[,n] <- as.numeric(l[,n]) } # Turn numeric
   
-  if (isTRUE(tax != 0)){ l$`After Tax` <- l[,ncol(l)] * (1 - tax) } # Tax
+  if (tax != 0){ l$`After Tax` <- l[,ncol(l)] * (1 - tax) } # Tax
   
   l$`Cumulative Total` <- cumsum(l[,ncol(l)]) # Column with Cumulative Total
   
