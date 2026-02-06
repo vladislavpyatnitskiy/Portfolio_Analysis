62672ae8842938985c999992be18a23d6df8c36b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-02-06 10:37:22 +0500

    Update Earning Dates.R
    Optimised script:
    
    • Improved script readability and reduced excessive characters

--- Scripts/Portfolio Securities Data/Earning Dates.R
+++ Scripts/Portfolio Securities Data/Earning Dates.R
@@ -8,23 +8,39 @@ p.earnings <- function(x, sort=T, desc=F, ts=F){ # Earning Dates for Portfolio
   
   for (n in 1:length(x)){ y <- x[n]
     
-    p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/",
-                           tolower(y))) %>% html_nodes('table') %>% .[[2]] %>%
-      html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    p <- read_html(
+      sprintf("https://stockanalysis.com/stocks/%s/", tolower(y))) %>% 
+      html_nodes('table') %>% .[[2]] %>% html_nodes('tr') %>% 
+      html_nodes('td') %>% html_text()
     
-    l <- c(l, format(as.Date(p[grep("Earnings Date", p) + 1],
-                             format = "%B %d, %Y"), "%Y-%m-%d")) }
+    message(
+      sprintf(
+        "%s data is downloaded (%s/%s)",
+        y, which(x == y), length(x)
+      )
+    )
+    
+    l <- c(
+      l, 
+      format(
+        as.Date(
+          p[grep("Earnings Date", p) + 1], format = "%B %d, %Y"
+          ), 
+        "%Y-%m-%d"
+        )
+    ) 
+    
+  }
   names(l) <- x # Tickers
   
-  if (isTRUE(sort)){ # If needed sorted, choose between asc and desc
-    
-    if (isTRUE(desc)){ l <- sort(l, decreasing = T) } else { l <- sort(l) } }
+  # If needed sorted, choose between asc and desc
+  if (sort){ if (desc){ l <- sort(l, decreasing = T) } else { l <- sort(l) } }
   
   l <- as.data.frame(l)
   
   colnames(l) <- "Earnings Date" # Earning Date column name
   
-  if (isTRUE(ts)){ U <- unique(l[,1]) # Find unique values for dates
+  if (ts){ U <- unique(l[,1]) # Find unique values for dates
     
     tickers <- rownames(l) # Assign new variable to contain values for tickers
     
@@ -36,10 +52,14 @@ p.earnings <- function(x, sort=T, desc=F, ts=F){ # Earning Dates for Portfolio
     
     for (n in 1:length(U)){ # Assign tickers to each date:
       
-      if (isFALSE(identical(names(which(d == U[n])), character(0)))){
+      if (!identical(names(which(d == U[n])), character(0))){
         
-        m <- rbind.data.frame(m, cbind(U[n],
-                                       toString(names(which(d == U[n]))))) } }
+        m <- rbind.data.frame(
+          m, cbind(U[n], toString(names(which(d == U[n]))))
+        )
+        
+        } 
+      }
     
     rownames(m) <- m[,1] # Assign dates as row names
     
