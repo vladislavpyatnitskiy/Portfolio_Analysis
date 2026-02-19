d28ce5ad9259d7d63bc4a8427dcdaf67903a3c30
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-02-19 13:18:57 +0500

    Optimize plot margins in dividend accumulation function
    Removed redundant par() call for plot margins.

--- Plots/Scripts for plots/Line Plot for Portfolio Dividends.R
+++ Plots/Scripts for plots/Line Plot for Portfolio Dividends.R
@@ -1,11 +1,11 @@
 p.plt.dividends <- function(x, c = "$US", sub = NULL){ # Dividend Accumulation
   
   x <- cumsum(x[,ncol(x)]) # Calculate cumulative sum of dividends
+
+  par(mar = c(5, 3.5, 5, 3.5)) # Define borders of the plot
   
   plot(x, main = sprintf("Dividend Amount Accumulation (in %s)", c), lwd = 3,
        col="red", las=1, ylab = "", xlab = "Trading Days", sub=sub, type = "l")
-  
-  par(mar = c(5, 3.5, 5, 3.5)) # Define borders of the plot
   
   abline(h = 0) # Add black horizontal line showing 0 and grey line for others
   
