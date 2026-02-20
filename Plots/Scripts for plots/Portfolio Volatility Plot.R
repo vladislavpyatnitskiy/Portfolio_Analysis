4a466e8397e03a6f70a98502d58ca8a0bb2dad12
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-02-20 21:19:58 +0500

    Update Portfolio Volatility Plot.R
    Updated script:
    
    • Improved readability of plot's part
    • Adjusted margins

--- Plots/Scripts for plots/Portfolio Volatility Plot.R
+++ Plots/Scripts for plots/Portfolio Volatility Plot.R
@@ -12,7 +12,7 @@ p.volatility.plt <- function(x, abs = F){ # Plot Security volatility
   
   # Loop for portfolio log returns calculation
   for (n in 2:ncol(x1)){ df2p <- x1[,(n-1):n] # x1 # Select two periods
-  
+    
     s <- df2p[apply(df2p, 1, function(row) all(row !=0 )),] # Remove zeros & NA
     
     r <- rbind(r, log(colSums(s)[2]/colSums(s)[1])) } # Add value to data frame
@@ -23,18 +23,27 @@ p.volatility.plt <- function(x, abs = F){ # Plot Security volatility
   x <- as.timeSeries(r) * 100 # Make it time series & multiply by 100
   
   main = ifelse(abs == T, "Volatility", "Fluctuations")
-  if (isTRUE(abs)){ x <- abs(x) }
+  
+  if (abs) x <- abs(x)
+  
+  par(mar = c(5, rep(4, 3))) # Margins
   
   for (n in 1:ncol(x)){ 
     
-    plot(x[,n], col = "red", ylab = "Returns (%)", xlab = "Trading Days",
-         main = sprintf("%s of Portfolio %s", main , colnames(x[,n])), las = 2,
-         ylim = c(min(x[,n]), max(x[,n]))) 
+    plot(
+      x[,n], 
+      col = "red", 
+      ylab = "Returns (%)", 
+      xlab = "Trading Days",
+      main = sprintf("%s of Portfolio %s", main , colnames(x[,n])), 
+      las = 2,
+      ylim = c(min(x[,n]), max(x[,n]))
+      ) 
     
     axis(side = 4, las = 2) # Right y axis and lines
     
     grid(nx = 1, ny = NULL, col = "grey", lty = "dotted", lwd = 1)
-    abline(h = 0) 
-    par(mar = c(5, 4, 4, 4)) } # Define borders of the plot to fit right y-axis
+    
+    abline(h = 0) } 
 }
 p.volatility.plt(df_portfolio, abs = T)
